a27819be7b7097c6bc770251724ba32a90f6a53a
3052 <spen+3052@protonmail.com>
2023-11-19 23:05:38 -0600

    +   "fmt"

--- dash.go
+++ dash.go
@@ -5,89 +5,61 @@ import (
    "encoding/hex"
    "encoding/xml"
    "errors"
-   "io"
-   "strconv"
+   "fmt"
    "strings"
 )
 
-func Adaptations(r io.Reader) ([]AdaptationSet, error) {
-   var s struct {
-      Period struct {
-         AdaptationSet []AdaptationSet
-      }
-   }
-   err := xml.NewDecoder(r).Decode(&s)
-   if err != nil {
-      return nil, err
-   }
-   for i, a := range s.Period.AdaptationSet {
-      for j, r := range a.Representation {
-         k := &s.Period.AdaptationSet[i].Representation[j]
-         if len(r.ContentProtection) == 0 {
-            k.ContentProtection = a.ContentProtection
-         }
-         if r.SegmentTemplate == nil {
-            k.SegmentTemplate = a.SegmentTemplate
-         }
-      }
-   }
-   return s.Period.AdaptationSet, nil
-}
-
-func (Representation) String() string {
+func (r Representation) String() string {
    var b []byte
    if r.Width >= 1 {
-      b = append(b, "width: "...)
-      b = strconv.AppendInt(b, r.Width, 10)
+      b = fmt.Append(b, "width: ", r.Width)
    }
    if r.Height >= 1 {
       if b != nil {
          b = append(b, '\n')
       }
-      b = append(b, "height: "...)
-      b = strconv.AppendInt(b, r.Height, 10)
+      b fmt.Append(b, "height: ", r.Height)
    }
    if r.Bandwidth >= 1 {
       if b != nil {
          b = append(b, '\n')
       }
-      b = append(b, "bandwidth: "...)
-      b = strconv.AppendInt(b, r.Bandwdith, 10)
+      b = fmt.Append(b, "bandwidth: ", r.Bandwidth)
    }
    if r.Codecs != "" {
       if b != nil {
          b = append(b, '\n')
       }
-      b = append(b, "codecs: "...)
-      b = append(b, r.Codecs...)
+      b = fmt.Append(b, "codecs: ", r.Codecs)
    }
-   s = append(s, "type: " + a.Type())
-   if a.Role != nil {
-      s = append(s, "role: " + a.Role.Value)
+   if b != nil {
+      b = append(b, '\n')
    }
-   if a.Lang != "" {
-      s = append(s, "language: " + a.Lang)
+   b = fmt.Append(b, "type: ", r.MimeType)
+   if v := r.adaptationset.Role; v != nil {
+      b = fmt.Append(b, "\nrole: ", v.Value)
    }
+   if r.adaptationSet.Lang != "" {
+      b = fmt.Append(b, "\nlanguage: ", r.adaptationSet.Lang)
+   }
+   return string(b)
 }
 
-type Representation struct {
-   Bandwidth int64 `xml:"bandwidth,attr"`
-   Codecs string `xml:"codecs,attr"`
-   ID string `xml:"id,attr"`
-   // this might not exist
-   BaseURL string
-   // this might be under AdaptationSet
+type AdaptationSet struct {
+   // this might be under Representation
    ContentProtection []ContentProtection
    // this might not exist
-   Height int64 `xml:"height,attr"`
-   // this might be under AdaptationSet
+   Lang string `xml:"lang,attr"`
+   // this might be under Representation
    MimeType string `xml:"mimeType,attr"`
+   // set want to edit these
+   Representation []*Representation
    // this might not exist
-   SegmentBase *SegmentBase
-   // this might not exist, or might be under AdaptationSet
+   Role *struct {
+      Value string `xml:"value,attr"`
+   }
+   // this might not exist, or might be under Representation
    SegmentTemplate *SegmentTemplate
-   // this might not exist
-   Width int64 `xml:"width,attr"`
 }
 
 type ContentProtection struct {
@@ -122,40 +94,103 @@ type SegmentBase struct {
    IndexRange string `xml:"indexRange,attr"`
 }
 
-func (s SegmentBase) Start() (int64, error) {
-   i := strings.Index(s.IndexRange, "-")
-   return strconv.ParseInt(s.IndexRange[:i], 10, 64)
+const widevine = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
+
+func (r Representation) Video() bool {
+   return r.MimeType == "video/mp4"
 }
 
-type AdaptationSet struct {
-   Representation []Representation
-   // this might be under Representation
+func (r Representation) Audio() bool {
+   return r.MimeType == "audio/mp4"
+}
+
+func Representations(b []byte) ([]*Representation, error) {
+   var s struct {
+      Period struct {
+         // this need to be pointer so we can avoid loop bug
+         AdaptationSet []*AdaptationSet
+      }
+   }
+   err := xml.Unmarshal(b, &s)
+   if err != nil {
+      return nil, err
+   }
+   for _, a := range s.Period.AdaptationSet {
+      for _, r := range a.Representation {
+         if len(r.ContentProtection) == 0 {
+            r.ContentProtection = a.ContentProtection
+         }
+         if r.MimeType == "" {
+            r.MimeType == a.MimeType
+         }
+         r.adaptationSet = a
+      }
+   }
+   return s.Period.AdaptationSet, nil
+}
+
+type Representation struct {
+   Bandwidth int `xml:"bandwidth,attr"`
+   Codecs string `xml:"codecs,attr"`
+   ID string `xml:"id,attr"`
+   adaptationSet *AdaptationSet
+   // this might not exist
+   BaseURL string
+   // this might be under AdaptationSet
    ContentProtection []ContentProtection
    // this might not exist
-   Lang string `xml:"lang,attr"`
-   // this might be under Representation
+   Height int `xml:"height,attr"`
+   // this might be under AdaptationSet
    MimeType string `xml:"mimeType,attr"`
    // this might not exist
-   Role *struct {
-      Value string `xml:"value,attr"`
-   }
-   // this might not exist, or might be under Representation
+   SegmentBase *SegmentBase
+   // this might not exist, or might be under AdaptationSet
    SegmentTemplate *SegmentTemplate
+   // this might not exist
+   Width int `xml:"width,attr"`
 }
 
-func (a AdaptationSet) Type() string {
-   if a.MimeType != "" {
-      return a.MimeType
+////////////////////////////////
+
+func (r Representation) Default_KID() ([]byte, error) {
+   for _, c := range r.ContentProtection {
+      if c.SchemeIdUri == "urn:mpeg:dash:mp4protection:2011" {
+         c.Default_KID = strings.ReplaceAll(c.Default_KID, "-", "")
+         return hex.DecodeString(c.Default_KID)
+      }
    }
-   return a.ContentType
+   return nil, errors.New("default_KID")
 }
 
-func (a AdaptationSet) Audio() bool {
-   return strings.HasPrefix(a.Type(), "audio")
+func (i Initialization) Replace(id string) string {
+   return strings.Replace(string(i), "$RepresentationID$", id, 1)
 }
 
-func (a AdaptationSet) Video() bool {
-   return strings.HasPrefix(a.Type(), "video")
+func (s SegmentBase) Start() (int64, error) {
+   var i int64
+   _, err := fmt.Sscan(s.IndexRange, &i)
+   if err != nil {
+      return 0, err
+   }
+   return i, nil
+}
+
+func (m Media) Replace(r Representation) []string {
+   var refs []string
+   for _, segment := range r.SegmentTemplate.SegmentTimeline.S {
+      segment.T = r.SegmentTemplate.StartNumber
+      for segment.R >= 0 {
+         ref := func(s string) string {
+            s = strings.Replace(s, "$Number$", fmt.Sprint(segment.T), 1)
+            return strings.Replace(s, "$RepresentationID$", r.ID, 1)
+         }(string(m))
+         refs = append(refs, ref)
+         r.SegmentTemplate.StartNumber++
+         segment.R--
+         segment.T++
+      }
+   }
+   return refs
 }
 
 func (a AdaptationSet) Ext() (string, bool) {
@@ -166,30 +201,6 @@ func (a AdaptationSet) Ext() (string, bool) {
       return ".m4v", true
    }
    return "", false
-}
-
-func (i Initialization) Replace(id string) string {
-   return strings.Replace(string(i), "$RepresentationID$", id, 1)
-}
-
-const widevine = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
-
-func (m Media) Replace(r Representation) []string {
-   var refs []string
-   for _, segment := range r.SegmentTemplate.SegmentTimeline.S {
-      segment.T = r.SegmentTemplate.StartNumber
-      for segment.R >= 0 {
-         ref := func(s string) string {
-            s = strings.Replace(s, "$Number$", strconv.Itoa(segment.T), 1)
-            return strings.Replace(s, "$RepresentationID$", r.ID, 1)
-         }(string(m))
-         refs = append(refs, ref)
-         r.SegmentTemplate.StartNumber++
-         segment.R--
-         segment.T++
-      }
-   }
-   return refs
 }
 
 func (a AdaptationSet) PSSH() ([]byte, error) {
@@ -206,14 +217,4 @@ func (a AdaptationSet) PSSH() ([]byte, error) {
       }
    }
    return nil, errors.New("pssh")
-}
-
-func (r Representation) Default_KID() ([]byte, error) {
-   for _, c := range r.ContentProtection {
-      if c.SchemeIdUri == "urn:mpeg:dash:mp4protection:2011" {
-         c.Default_KID = strings.ReplaceAll(c.Default_KID, "-", "")
-         return hex.DecodeString(c.Default_KID)
-      }
-   }
-   return nil, errors.New("default_KID")
 }
