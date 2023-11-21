d5f39a370dcf1f6917c890820d02864b3b7e6e00
3052 <spen+3052@protonmail.com>
2023-11-20 19:31:56 -0600

    +      b = fmt.Append(b, "height: ", r.Height)

--- dash.go
+++ dash.go
@@ -18,7 +18,7 @@ func (r Representation) String() string {
       if b != nil {
          b = append(b, '\n')
       }
-      b fmt.Append(b, "height: ", r.Height)
+      b = fmt.Append(b, "height: ", r.Height)
    }
    if r.Bandwidth >= 1 {
       if b != nil {
@@ -36,13 +36,120 @@ func (r Representation) String() string {
       b = append(b, '\n')
    }
    b = fmt.Append(b, "type: ", r.MimeType)
-   if v := r.adaptationset.Role; v != nil {
+   if v := r.adaptationSet.Role; v != nil {
       b = fmt.Append(b, "\nrole: ", v.Value)
    }
    if r.adaptationSet.Lang != "" {
       b = fmt.Append(b, "\nlanguage: ", r.adaptationSet.Lang)
    }
    return string(b)
+}
+
+func Representations(b []byte) ([]*Representation, error) {
+   var s struct {
+      Period struct {
+         // this need to be pointer so we can avoid loop bug
+         AdaptationSet []*AdaptationSet
+      }
+   }
+   err := xml.Unmarshal(b, &s)
+   if err != nil {
+      return nil, err
+   }
+   var rs []*Representation
+   for _, a := range s.Period.AdaptationSet {
+      for _, r := range a.Representation {
+         if len(r.ContentProtection) == 0 {
+            r.ContentProtection = a.ContentProtection
+         }
+         if r.MimeType == "" {
+            r.MimeType = a.MimeType
+         }
+         r.adaptationSet = a
+         rs = append(rs, r)
+      }
+   }
+   return rs, nil
+}
+
+func (r Representation) Default_KID() ([]byte, error) {
+   for _, c := range r.ContentProtection {
+      if c.SchemeIdUri == "urn:mpeg:dash:mp4protection:2011" {
+         c.Default_KID = strings.ReplaceAll(c.Default_KID, "-", "")
+         return hex.DecodeString(c.Default_KID)
+      }
+   }
+   return nil, errors.New("default_KID")
+}
+
+func (r Representation) PSSH() ([]byte, error) {
+   for _, c := range r.ContentProtection {
+      if c.SchemeIdUri == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed" {
+         return base64.StdEncoding.DecodeString(c.PSSH)
+      }
+   }
+   return nil, errors.New("PSSH")
+}
+
+func (r Representation) Video() bool {
+   return r.MimeType == "video/mp4"
+}
+
+func (r Representation) Audio() bool {
+   return r.MimeType == "audio/mp4"
+}
+
+func (r Representation) Ext() (string, bool) {
+   switch {
+   case r.Audio():
+      return ".m4a", true
+   case r.Video():
+      return ".m4v", true
+   }
+   return "", false
+}
+
+func (r Representation) Index() (int64, error) {
+   var i int64
+   _, err := fmt.Sscan(r.SegmentBase.IndexRange, &i)
+   if err != nil {
+      return 0, err
+   }
+   return i, nil
+}
+
+func (r Representation) Initialization() string {
+   return strings.Replace(
+      r.SegmentTemplate.Initialization, "$RepresentationID$", r.ID, 1,
+   )
+}
+
+func (r Representation) Media() []string {
+   var refs []string
+   for _, segment := range r.SegmentTemplate.SegmentTimeline.S {
+      segment.T = r.SegmentTemplate.StartNumber
+      for segment.R >= 0 {
+         ref := func(s string) string {
+            s = strings.Replace(s, "$Number$", fmt.Sprint(segment.T), 1)
+            return strings.Replace(s, "$RepresentationID$", r.ID, 1)
+         }(r.SegmentTemplate.Media)
+         refs = append(refs, ref)
+         r.SegmentTemplate.StartNumber++
+         segment.R--
+         segment.T++
+      }
+   }
+   return refs
+}
+
+////////////////////////////////////////
+
+type ContentProtection struct {
+   SchemeIdUri string `xml:"schemeIdUri,attr"`
+   // this might not exist
+   Default_KID string `xml:"default_KID,attr"`
+   // this might not exist
+   PSSH string `xml:"pssh"`
 }
 
 type AdaptationSet struct {
@@ -62,73 +169,6 @@ type AdaptationSet struct {
    SegmentTemplate *SegmentTemplate
 }
 
-type ContentProtection struct {
-   SchemeIdUri string `xml:"schemeIdUri,attr"`
-   // this might not exist
-   Default_KID string `xml:"default_KID,attr"`
-   // this might not exist
-   PSSH string `xml:"pssh"`
-}
-
-type SegmentTemplate struct {
-   Initialization Initialization `xml:"initialization,attr"`
-   Media Media `xml:"media,attr"`
-   SegmentTimeline struct {
-      S []struct {
-         // duration
-         D int `xml:"d,attr"`
-         // repeat. this may not exist
-         R int `xml:"r,attr"`
-         // time. this may not exist
-         T int `xml:"t,attr"`
-      }
-   }
-   StartNumber int `xml:"startNumber,attr"`
-}
-
-type Initialization string
-
-type Media string
-
-type SegmentBase struct {
-   IndexRange string `xml:"indexRange,attr"`
-}
-
-const widevine = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
-
-func (r Representation) Video() bool {
-   return r.MimeType == "video/mp4"
-}
-
-func (r Representation) Audio() bool {
-   return r.MimeType == "audio/mp4"
-}
-
-func Representations(b []byte) ([]*Representation, error) {
-   var s struct {
-      Period struct {
-         // this need to be pointer so we can avoid loop bug
-         AdaptationSet []*AdaptationSet
-      }
-   }
-   err := xml.Unmarshal(b, &s)
-   if err != nil {
-      return nil, err
-   }
-   for _, a := range s.Period.AdaptationSet {
-      for _, r := range a.Representation {
-         if len(r.ContentProtection) == 0 {
-            r.ContentProtection = a.ContentProtection
-         }
-         if r.MimeType == "" {
-            r.MimeType == a.MimeType
-         }
-         r.adaptationSet = a
-      }
-   }
-   return s.Period.AdaptationSet, nil
-}
-
 type Representation struct {
    Bandwidth int `xml:"bandwidth,attr"`
    Codecs string `xml:"codecs,attr"`
@@ -142,79 +182,28 @@ type Representation struct {
    Height int `xml:"height,attr"`
    // this might be under AdaptationSet
    MimeType string `xml:"mimeType,attr"`
-   // this might not exist
-   SegmentBase *SegmentBase
    // this might not exist, or might be under AdaptationSet
    SegmentTemplate *SegmentTemplate
    // this might not exist
    Width int `xml:"width,attr"`
+   // this might not exist
+   SegmentBase *struct {
+      IndexRange string `xml:"indexRange,attr"`
+   }
 }
 
-func (r Representation) Default_KID() ([]byte, error) {
-   for _, c := range r.ContentProtection {
-      if c.SchemeIdUri == "urn:mpeg:dash:mp4protection:2011" {
-         c.Default_KID = strings.ReplaceAll(c.Default_KID, "-", "")
-         return hex.DecodeString(c.Default_KID)
+type SegmentTemplate struct {
+   SegmentTimeline struct {
+      S []struct {
+         // duration
+         D int `xml:"d,attr"`
+         // repeat. this may not exist
+         R int `xml:"r,attr"`
+         // time. this may not exist
+         T int `xml:"t,attr"`
       }
    }
-   return nil, errors.New("default_KID")
-}
-
-////////////////////////////////
-
-func (i Initialization) Replace(id string) string {
-   return strings.Replace(string(i), "$RepresentationID$", id, 1)
-}
-
-func (s SegmentBase) Start() (int64, error) {
-   var i int64
-   _, err := fmt.Sscan(s.IndexRange, &i)
-   if err != nil {
-      return 0, err
-   }
-   return i, nil
-}
-
-func (m Media) Replace(r Representation) []string {
-   var refs []string
-   for _, segment := range r.SegmentTemplate.SegmentTimeline.S {
-      segment.T = r.SegmentTemplate.StartNumber
-      for segment.R >= 0 {
-         ref := func(s string) string {
-            s = strings.Replace(s, "$Number$", fmt.Sprint(segment.T), 1)
-            return strings.Replace(s, "$RepresentationID$", r.ID, 1)
-         }(string(m))
-         refs = append(refs, ref)
-         r.SegmentTemplate.StartNumber++
-         segment.R--
-         segment.T++
-      }
-   }
-   return refs
-}
-
-func (a AdaptationSet) Ext() (string, bool) {
-   switch {
-   case a.Audio():
-      return ".m4a", true
-   case a.Video():
-      return ".m4v", true
-   }
-   return "", false
-}
-
-func (a AdaptationSet) PSSH() ([]byte, error) {
-   for _, c := range a.ContentProtection {
-      if c.SchemeIdUri == widevine {
-         return base64.StdEncoding.DecodeString(c.PSSH)
-      }
-   }
-   for _, r := range a.Representation {
-      for _, c := range r.ContentProtection {
-         if c.SchemeIdUri == widevine {
-            return base64.StdEncoding.DecodeString(c.PSSH)
-         }
-      }
-   }
-   return nil, errors.New("pssh")
+   StartNumber int `xml:"startNumber,attr"`
+   Initialization string `xml:"initialization,attr"`
+   Media string `xml:"media,attr"`
 }
