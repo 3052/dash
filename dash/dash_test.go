c6bf8cbedd01582ef57e799acef77839a89b1676
3052 <spen+3052@protonmail.com>
2023-12-31 21:15:19 -0600

    +  <Period id="ad-bumper-0-0" duration="PT5.005S" start="PT0.000S"

--- dash/dash_test.go
+++ dash/dash_test.go
@@ -3,29 +3,11 @@ package dash
 import (
    "fmt"
    "net/http"
-   "os"
    "testing"
 )
 
-func read_file(name string) ([]*Representation, error) {
-   file, err := os.Open(name)
-   if err != nil {
-      return nil, err
-   }
-   defer file.Close()
-   return Representations(file)
-}
-
-var tests = []string{
-   "mpd/amc.mpd",
-   "mpd/hulu.mpd",
-   "mpd/nbc.mpd",
-   "mpd/paramount.mpd",
-   "mpd/roku.mpd",
-}
-
 func Test_Ext(t *testing.T) {
-   for _, name := range tests {
+   for name := range tests {
       reps, err := read_file(name)
       if err != nil {
          t.Fatal(err)
@@ -39,20 +21,13 @@ func Test_Ext(t *testing.T) {
    }
 }
 
-func Test_Info(t *testing.T) {
-   for _, name := range tests {
-      reps, err := read_file(name)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(name)
-      for i, rep := range reps {
-         if i >= 1 {
-            fmt.Println()
-         }
-         fmt.Println(rep)
-      }
-      fmt.Println()
+func Test_SegmentBase(t *testing.T) {
+   reps, err := read_file("mpd/hulu.mpd")
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, rep := range reps {
+      fmt.Println(rep.Sidx_Moof())
    }
 }
 
