acdaf842c476f8c4f5dd1b5aebe4a40f2fa17873
3052 <spen+3052@protonmail.com>
2024-02-10 15:59:51 -0600

    -func (m Media) String() string {

--- hls/hls.go
+++ hls/hls.go
@@ -19,21 +19,6 @@ type Media struct {
    RawUri string
 }
 
-func (m Media) String() string {
-   var b strings.Builder
-   b.WriteString("group ID: ")
-   b.WriteString(m.GroupId)
-   b.WriteString("\ntype: ")
-   b.WriteString(m.Type)
-   b.WriteString("\nname: ")
-   b.WriteString(m.Name)
-   if m.Characteristics != "" {
-      b.WriteString("\ncharacteristics: ")
-      b.WriteString(m.Characteristics)
-   }
-   return b.String()
-}
-
 type Stream struct {
    Bandwidth int64
    RawUri string
@@ -42,25 +27,6 @@ type Stream struct {
    Resolution string
 }
 
-func (m Stream) String() string {
-   var b []byte
-   if m.Resolution != "" {
-      b = append(b, "resolution: "...)
-      b = append(b, m.Resolution...)
-      b = append(b, '\n')
-   }
-   b = append(b, "bandwidth: "...)
-   b = strconv.AppendInt(b, m.Bandwidth, 10)
-   if m.Codecs != "" {
-      b = append(b, "\ncodecs: "...)
-      b = append(b, m.Codecs...)
-   }
-   if m.Audio != "" {
-      b = append(b, "\naudio: "...)
-      b = append(b, m.Audio...)
-   }
-   return string(b)
-}
 type Block struct {
    cipher.Block
    key []byte
@@ -153,130 +119,6 @@ func (Media) Ext() string {
    return ".m4a"
 }
 
-func (m Media) URI() string {
-   return m.RawUri
-}
-
 func (Stream) Ext() string {
    return ".m4v"
-}
-
-func (m Stream) URI() string {
-   return m.RawUri
-}
-
-func (s Scanner) Master() (*Master, error) {
-   var mas Master
-   for s.line.Scan() != scanner.EOF {
-      var err error
-      line := s.line.TokenText()
-      s.Init(strings.NewReader(line))
-      switch {
-      // rfc-editor.org/rfc/rfc8216#section-4.3.4.1
-      case strings.HasPrefix(line, "#EXT-X-MEDIA:"):
-         var med Media
-         for s.Scan() != scanner.EOF {
-            switch s.TokenText() {
-            case "CHARACTERISTICS":
-               s.Scan()
-               s.Scan()
-               med.Characteristics, err = strconv.Unquote(s.TokenText())
-            case "GROUP-ID":
-               s.Scan()
-               s.Scan()
-               med.GroupId, err = strconv.Unquote(s.TokenText())
-            case "NAME":
-               s.Scan()
-               s.Scan()
-               med.Name, err = strconv.Unquote(s.TokenText())
-            case "TYPE":
-               s.Scan()
-               s.Scan()
-               med.Type = s.TokenText()
-            case "URI":
-               s.Scan()
-               s.Scan()
-               med.RawUri, err = strconv.Unquote(s.TokenText())
-            }
-            if err != nil {
-               return nil, err
-            }
-         }
-         mas.Media = append(mas.Media, med)
-      case strings.HasPrefix(line, "#EXT-X-STREAM-INF:"):
-         var str Stream
-         for s.Scan() != scanner.EOF {
-            switch s.TokenText() {
-            case "AUDIO":
-               s.Scan()
-               s.Scan()
-               str.Audio, err = strconv.Unquote(s.TokenText())
-            case "BANDWIDTH":
-               s.Scan()
-               s.Scan()
-               str.Bandwidth, err = strconv.ParseInt(s.TokenText(), 10, 64)
-            case "CODECS":
-               s.Scan()
-               s.Scan()
-               str.Codecs, err = strconv.Unquote(s.TokenText())
-            case "RESOLUTION":
-               s.Scan()
-               s.Scan()
-               str.Resolution = s.TokenText()
-            }
-            if err != nil {
-               return nil, err
-            }
-         }
-         s.line.Scan()
-         str.RawUri = s.line.TokenText()
-         mas.Stream = append(mas.Stream, str)
-      }
-   }
-   return &mas, nil
-}
-
-type Mixed interface {
-   Ext() string
-   URI() string
-}
-
-type Master struct {
-   Media []Media
-   Stream []Stream
-}
-
-type Scanner struct {
-   line scanner.Scanner
-   scanner.Scanner
-}
-
-func NewScanner(body io.Reader) Scanner {
-   var scan Scanner
-   scan.line.Init(body)
-   scan.line.IsIdentRune = func(r rune, i int) bool {
-      if r == '\n' {
-         return false
-      }
-      if r == '\r' {
-         return false
-      }
-      if r == scanner.EOF {
-         return false
-      }
-      return true
-   }
-   scan.IsIdentRune = func(r rune, i int) bool {
-      if r == '-' {
-         return true
-      }
-      if unicode.IsDigit(r) {
-         return true
-      }
-      if unicode.IsLetter(r) {
-         return true
-      }
-      return false
-   }
-   return scan
 }
