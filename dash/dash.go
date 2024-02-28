0b923f2450adfb71272033868bce639782e4fcf5
3052 <spen+3052@protonmail.com>
2024-02-27 20:12:54 -0600

    +type Representation struct {

--- dash/dash.go
+++ dash/dash.go
@@ -19,6 +19,33 @@ func (r Range) Scan() (int, int, error) {
    return start, end, nil
 }
 
+type Representation struct {
+   adaptation_set *adaptation_set
+   Bandwidth int `xml:"bandwidth,attr"`
+   ID string `xml:"id,attr"`
+   // this might not exist
+   BaseURL string
+   // this might not exist, or might be under AdaptationSet
+   Codecs string `xml:"codecs,attr"`
+   // this might be under AdaptationSet
+   ContentProtection []ContentProtection
+   // this might not exist
+   Height int `xml:"height,attr"`
+   // this might be under AdaptationSet
+   MimeType string `xml:"mimeType,attr"`
+   // this might not exist
+   SegmentBase *struct {
+      Initialization struct {
+         Range Range `xml:"range,attr"`
+      }
+      IndexRange Range `xml:"indexRange,attr"`
+   }
+   // this might not exist, or might be under AdaptationSet
+   SegmentTemplate *SegmentTemplate
+   // this might not exist
+   Width int `xml:"width,attr"`
+}
+
 type ContentProtection struct {
    SchemeIdUri string `xml:"schemeIdUri,attr"`
    // this might not exist
@@ -64,50 +91,6 @@ type adaptation_set struct {
 type period struct {
    AdaptationSet []adaptation_set
    ID string `xml:"id,attr"`
-}
-
-type Representation struct {
-   adaptation_set *adaptation_set
-   Bandwidth int `xml:"bandwidth,attr"`
-   ID string `xml:"id,attr"`
-   // this might not exist
-   BaseURL string
-   // this might not exist, or might be under AdaptationSet
-   Codecs string `xml:"codecs,attr"`
-   // this might be under AdaptationSet
-   ContentProtection []ContentProtection
-   // this might not exist
-   Height int `xml:"height,attr"`
-   // this might be under AdaptationSet
-   MimeType string `xml:"mimeType,attr"`
-   // this might not exist
-   SegmentBase *struct {
-      Initialization struct {
-         Range Range `xml:"range,attr"`
-      }
-      IndexRange Range `xml:"indexRange,attr"`
-   }
-   // this might not exist, or might be under AdaptationSet
-   SegmentTemplate *SegmentTemplate
-   // this might not exist
-   Width int `xml:"width,attr"`
-}
-
-func (r Representation) codecs() (string, bool) {
-   if v := r.Codecs; v != "" {
-      return v, true
-   }
-   if v := r.adaptation_set.Codecs; v != "" {
-      return v, true
-   }
-   return "", false
-}
-
-func (r Representation) mime_type() string {
-   if v := r.MimeType; v != "" {
-      return v
-   }
-   return v.adaptation_set.MimeType
 }
 
 func (r Representation) Ext() (string, bool) {
@@ -194,73 +177,74 @@ func (r Representation) Media() []string {
    return media
 }
 
-///////////////
+func (r Representation) codecs() (string, bool) {
+   if v := r.Codecs; v != "" {
+      return v, true
+   }
+   if v := r.adaptation_set.Codecs; v != "" {
+      return v, true
+   }
+   return "", false
+}
 
-func (m MPD) String() string {
+func (r Representation) mime_type() string {
+   if v := r.MimeType; v != "" {
+      return v
+   }
+   return v.adaptation_set.MimeType
+}
+
+func (r Representation) String() string {
    var b []byte
-   m.Visit(func(p Pointer) {
+   if v := r.Width; v >= 1 {
+      b = fmt.Append(b, "width = ", v)
+   }
+   if v := r.Height; v >= 1 {
       if b != nil {
-         b = append(b, "\n\n"...)
+         b = append(b, '\n')
       }
-      var c []byte
-      if v := p.Representation.Width; v >= 1 {
-         c = fmt.Append(c, "width = ", v)
-      }
-      if v := p.Representation.Height; v >= 1 {
-         if c != nil {
-            c = append(c, '\n')
-         }
-         c = fmt.Append(c, "height = ", v)
-      }
-      if c != nil {
-         c = append(c, '\n')
-      }
-      c = fmt.Append(c, "bandwidth = ", p.Representation.Bandwidth)
-      if v, ok := p.codecs(); ok {
-         c = fmt.Append(c, "\ncodecs = ", v)
-      }
-      c = fmt.Append(c, "\ntype = ", p.mime_type())
-      if v := p.adaptation_set.Role; v != nil {
-         c = fmt.Append(c, "\nrole = ", v.Value)
-      }
-      if v := p.adaptation_set.Lang; v != "" {
-         c = fmt.Append(c, "\nlang = ", v)
-      }
-      c = fmt.Append(c, "\nid = ", p.Representation.ID)
-      if v := p.period.ID; v != "" {
-         c = fmt.Append(c, "\nperiod = ", v)
-      }
-      b = append(b, c...)
-   })
+      b = fmt.Append(b, "height = ", v)
+   }
+   if b != nil {
+      b = append(b, '\n')
+   }
+   b = fmt.Append(b, "bandwidth = ", r.Bandwidth)
+   if v, ok := r.codecs(); ok {
+      b = fmt.Append(b, "\ncodecs = ", v)
+   }
+   b = fmt.Append(b, "\ntype = ", r.mime_type())
+   if v := r.adaptation_set.Role; v != nil {
+      b = fmt.Append(b, "\nrole = ", v.Value)
+   }
+   if v := r.adaptation_set.Lang; v != "" {
+      b = fmt.Append(b, "\nlang = ", v)
+   }
+   b = fmt.Append(b, "\nid = ", r.ID)
+   if v := r.adaptation_set.period.ID; v != "" {
+      b = fmt.Append(b, "\nperiod = ", v)
+   }
    return string(b)
 }
 
 // media presentation description
 // wikipedia.org/wiki/Dynamic_Adaptive_Streaming_over_HTTP
-type MPD struct {
-   Period []period
-}
-
-func (m MPD) Contains(f func(Pointer) bool) bool {
-   for _, period := range m.Period {
-      for _, adapt := range period.AdaptationSet {
-         for _, represent := range adapt.Representation {
-            var p Pointer
-            p.adaptation_set = &adapt
-            p.period = &period
-            p.Representation = &represent
-            if f(p) {
-               return true
-            }
+func Unmarshal(b []byte) ([]Representation, error) {
+   var s struct {
+      Period []period
+   }
+   err := xml.Unmarshal(b, &s)
+   if err != nil {
+      return nil, err
+   }
+   var rs []Representation
+   for _, p := range s.Period {
+      for _, a := range p.AdaptationSet {
+         a.period = &p
+         for _, r := range a.Representation {
+            r.adaptation_set = &a
+            rs = append(rs, r)
          }
       }
    }
-   return false
-}
-
-func (m MPD) Visit(f func(Pointer)) {
-   m.Contains(func(p Pointer) bool {
-      f(p)
-      return false
-   })
+   return rs, nil
 }
