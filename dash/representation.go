f3ec3ae62fbec3bb61d3e1825a65be5f80d22eb7
3052 <spen+3052@protonmail.com>
2024-02-27 20:46:39 -0600

    new file:   dash/representation.go

---
+++ dash/representation.go
@@ -0,0 +1,193 @@
+package dash
+
+import (
+   "encoding/base64"
+   "encoding/hex"
+   "encoding/xml"
+   "errors"
+   "fmt"
+   "strings"
+)
+
+type Representation struct {
+   adaptation_set *adaptation_set
+   Bandwidth int `xml:"bandwidth,attr"`
+   ID string `xml:"id,attr"`
+   // this might not exist
+   BaseURL string
+   // this might not exist, or might be under AdaptationSet
+   Codecs string `xml:"codecs,attr"`
+   // this might be under AdaptationSet
+   ContentProtection []ContentProtection
+   // this might not exist
+   Height int `xml:"height,attr"`
+   // this might be under AdaptationSet
+   MimeType string `xml:"mimeType,attr"`
+   // this might not exist
+   SegmentBase *struct {
+      Initialization struct {
+         Range Range `xml:"range,attr"`
+      }
+      IndexRange Range `xml:"indexRange,attr"`
+   }
+   // this might not exist, or might be under AdaptationSet
+   SegmentTemplate *SegmentTemplate
+   // this might not exist
+   Width int `xml:"width,attr"`
+}
+
+func (r Representation) Ext() (string, bool) {
+   switch r.mime_type() {
+   case "audio/mp4":
+      return ".m4a", true
+   case "video/mp4":
+      return ".m4v", true
+   }
+   return "", false
+}
+
+func (r Representation) segment_template() (*SegmentTemplate, bool) {
+   if v := r.SegmentTemplate; v != nil {
+      return v, true
+   }
+   if v := r.adaptation_set.SegmentTemplate; v != nil {
+      return v, true
+   }
+   return nil, false
+}
+
+func (r Representation) content_protection() []ContentProtection {
+   if v := r.ContentProtection; v != nil {
+      return v
+   }
+   return r.adaptation_set.ContentProtection
+}
+
+func (r Representation) Default_KID() ([]byte, error) {
+   for _, c := range r.content_protection() {
+      if c.SchemeIdUri == "urn:mpeg:dash:mp4protection:2011" {
+         c.Default_KID = strings.ReplaceAll(c.Default_KID, "-", "")
+         return hex.DecodeString(c.Default_KID)
+      }
+   }
+   return nil, errors.New("Representation.Default_KID")
+}
+
+func (r Representation) PSSH() ([]byte, error) {
+   for _, c := range r.content_protection() {
+      if c.SchemeIdUri == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed" {
+         if c.PSSH != "" {
+            return base64.StdEncoding.DecodeString(c.PSSH)
+         }
+      }
+   }
+   return nil, errors.New("Representation.PSSH")
+}
+
+func (r Representation) Initialization() (string, bool) {
+   if st, ok := r.segment_template(); ok {
+      if i := st.Initialization; i != "" {
+         return strings.Replace(i, "$RepresentationID$", r.ID, 1), true
+      }
+   }
+   return "", false
+}
+
+func (r Representation) Media() []string {
+   st, ok := r.segment_template()
+   if !ok {
+      return nil
+   }
+   replace := func(s, old string) string {
+      s = strings.Replace(s, "$RepresentationID$", r.ID, 1)
+      return strings.Replace(s, old, fmt.Sprint(st.StartNumber), 1)
+   }
+   var media []string
+   for _, segment := range st.SegmentTimeline.S {
+      for segment.R >= 0 {
+         var medium string
+         if strings.Contains(st.Media, "$Time$") {
+            medium = replace(st.Media, "$Time$")
+            st.StartNumber += segment.D
+         } else {
+            medium = replace(st.Media, "$Number$")
+            st.StartNumber++
+         }
+         media = append(media, medium)
+         segment.R--
+      }
+   }
+   return media
+}
+
+func (r Representation) codecs() (string, bool) {
+   if v := r.Codecs; v != "" {
+      return v, true
+   }
+   if v := r.adaptation_set.Codecs; v != "" {
+      return v, true
+   }
+   return "", false
+}
+
+func (r Representation) mime_type() string {
+   if v := r.MimeType; v != "" {
+      return v
+   }
+   return r.adaptation_set.MimeType
+}
+
+func (r Representation) String() string {
+   var b []byte
+   if v := r.Width; v >= 1 {
+      b = fmt.Append(b, "width = ", v)
+   }
+   if v := r.Height; v >= 1 {
+      if b != nil {
+         b = append(b, '\n')
+      }
+      b = fmt.Append(b, "height = ", v)
+   }
+   if b != nil {
+      b = append(b, '\n')
+   }
+   b = fmt.Append(b, "bandwidth = ", r.Bandwidth)
+   if v, ok := r.codecs(); ok {
+      b = fmt.Append(b, "\ncodecs = ", v)
+   }
+   b = fmt.Append(b, "\ntype = ", r.mime_type())
+   if v := r.adaptation_set.Role; v != nil {
+      b = fmt.Append(b, "\nrole = ", v.Value)
+   }
+   if v := r.adaptation_set.Lang; v != "" {
+      b = fmt.Append(b, "\nlang = ", v)
+   }
+   b = fmt.Append(b, "\nid = ", r.ID)
+   if v := r.adaptation_set.period.ID; v != "" {
+      b = fmt.Append(b, "\nperiod = ", v)
+   }
+   return string(b)
+}
+
+// media presentation description
+// wikipedia.org/wiki/Dynamic_Adaptive_Streaming_over_HTTP
+func Unmarshal(b []byte) ([]Representation, error) {
+   var s struct {
+      Period []period
+   }
+   err := xml.Unmarshal(b, &s)
+   if err != nil {
+      return nil, err
+   }
+   var rs []Representation
+   for _, p := range s.Period {
+      for _, a := range p.AdaptationSet {
+         a.period = &p
+         for _, r := range a.Representation {
+            r.adaptation_set = &a
+            rs = append(rs, r)
+         }
+      }
+   }
+   return rs, nil
+}
---
+++ dash/representation_test.go
@@ -0,0 +1,90 @@
+package dash
+
+import (
+   "encoding/xml"
+   "fmt"
+   "os"
+   "testing"
+)
+
+func TestString(t *testing.T) {
+   for i, name := range tests {
+      if i >= 1 {
+         fmt.Println()
+      }
+      fmt.Println(name)
+      text, err := os.ReadFile(name)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var media MPD
+      if err := xml.Unmarshal(text, &media); err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(media)
+   }
+}
+
+func TestMedia(t *testing.T) {
+   tests := [][]string{
+      // startNumber == nil
+      {"mpd/mubi.mpd", "dallas-edge3.mubicdn.net/stream/7a947d0bd29b2e5c17ec36497b1c67e4/65d5e449/da8710c0/mubi-films/325455/passages_eng_zxx_1800x1080_50000_mezz40828/ae8c88ed4e/drm_playlist.0ff148ef80.ism/default/"},
+      // startNumber == 0
+      {"mpd/amc.mpd", ""},
+      // startNumber == 1
+      {"mpd/paramount.mpd", "vod-gcs-cedexis.cbsaavideo.com/intl_vms/2022/02/24/2006197315671/77016_cenc_dash/"},
+   }
+   for _, test := range tests {
+      fmt.Println(test[0] + ":")
+      media, err := reader(test[0])
+      if err != nil {
+         t.Fatal(err)
+      }
+      media.Contains(func(p Pointer) bool {
+         for _, medium := range p.Media() {
+            fmt.Println(test[1] + medium)
+         }
+         return true
+      })
+   }
+}
+
+func TestInitialization(t *testing.T) {
+   media, err := reader("mpd/amc.mpd")
+   if err != nil {
+      t.Fatal(err)
+   }
+   media.Visit(func(p Pointer) {
+      v, ok := p.Initialization()
+      fmt.Printf("%v %q %v\n\n", p.Representation.ID, v, ok)
+   })
+}
+
+var tests = []string{
+   "mpd/amc.mpd",
+   "mpd/hulu.mpd",
+   "mpd/nbc.mpd",
+   "mpd/paramount.mpd",
+   "mpd/roku.mpd",
+}
+
+func TestProtection(t *testing.T) {
+   for _, test := range tests {
+      text, err := os.ReadFile(test)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var media MPD
+      if err := xml.Unmarshal(text, &media); err != nil {
+         t.Fatal(err)
+      }
+      media.Visit(func(p Pointer) {
+         _, pssh := p.PSSH()
+         _, kid := p.Default_KID()
+         fmt.Printf(
+            "mpd:%v period:%q type:%v pssh:%v kid:%v\n",
+            test, p.Period.ID, p.mime_type(), pssh, kid,
+         )
+      })
+   }
+}
