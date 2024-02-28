50bd7c7a54dc68b3b7572ec5ea3f6cd0ab0ea59c
3052 <spen+3052@protonmail.com>
2024-02-27 22:24:11 -0600

    new file:   dash/mpd/peacock.mpd

--- dash/dash_test.go
+++ dash/dash_test.go
@@ -1,34 +1,28 @@
 package dash
 
 import (
-   "encoding/xml"
    "fmt"
    "os"
    "testing"
 )
 
-func TestRange(t *testing.T) {
-   media, err := reader("mpd/hulu.mpd")
-   if err != nil {
-      t.Fatal(err)
-   }
-   media.Visit(func(p Pointer) {
-      sb := p.Representation.SegmentBase
-      start, end, err := sb.Initialization.Range.Scan()
-      fmt.Printf("%v %v %v ", start, end, err)
-      start, end, err = sb.IndexRange.Scan()
-      fmt.Printf("%v %v %v\n", start, end, err)
-   })
-}
-
-func reader(name string) (*MPD, error) {
+func reader(name string) ([]Representation, error) {
    text, err := os.ReadFile(name)
    if err != nil {
       return nil, err
    }
-   media := new(MPD)
-   if err := xml.Unmarshal(text, &media); err != nil {
-      return nil, err
+   return Unmarshal(text)
+}
+
+func TestRange(t *testing.T) {
+   reps, err := reader("mpd/hulu.mpd")
+   if err != nil {
+      t.Fatal(err)
    }
-   return media, nil
+   for _, rep := range reps {
+      start, end, err := rep.SegmentBase.Initialization.Range.Scan()
+      fmt.Printf("%v %v %v ", start, end, err)
+      start, end, err = rep.SegmentBase.IndexRange.Scan()
+      fmt.Printf("%v %v %v\n", start, end, err)
+   }
 }
--- dash/representation_test.go
+++ dash/representation_test.go
@@ -1,34 +1,17 @@
 package dash
 
 import (
-   "encoding/xml"
    "fmt"
    "os"
    "testing"
 )
 
-func TestMedia(t *testing.T) {
-   tests := [][]string{
-      // startNumber == nil
-      {"mpd/mubi.mpd", "dallas-edge3.mubicdn.net/stream/7a947d0bd29b2e5c17ec36497b1c67e4/65d5e449/da8710c0/mubi-films/325455/passages_eng_zxx_1800x1080_50000_mezz40828/ae8c88ed4e/drm_playlist.0ff148ef80.ism/default/"},
-      // startNumber == 0
-      {"mpd/amc.mpd", ""},
-      // startNumber == 1
-      {"mpd/paramount.mpd", "vod-gcs-cedexis.cbsaavideo.com/intl_vms/2022/02/24/2006197315671/77016_cenc_dash/"},
-   }
-   for _, test := range tests {
-      fmt.Println(test[0] + ":")
-      media, err := reader(test[0])
-      if err != nil {
-         t.Fatal(err)
-      }
-      media.Contains(func(p Pointer) bool {
-         for _, medium := range p.Media() {
-            fmt.Println(test[1] + medium)
-         }
-         return true
-      })
-   }
+var tests = []string{
+   "mpd/amc.mpd",
+   "mpd/hulu.mpd",
+   "mpd/nbc.mpd",
+   "mpd/paramount.mpd",
+   "mpd/roku.mpd",
 }
 
 func TestString(t *testing.T) {
@@ -41,50 +24,65 @@ func TestString(t *testing.T) {
       if err != nil {
          t.Fatal(err)
       }
-      var media MPD
-      if err := xml.Unmarshal(text, &media); err != nil {
+      reps, err := Unmarshal(text)
+      if err != nil {
          t.Fatal(err)
       }
-      fmt.Println(media)
+      for i, rep := range reps {
+         if i >= 1 {
+            fmt.Println()
+         }
+         fmt.Println(rep)
+      }
    }
 }
 
 func TestInitialization(t *testing.T) {
-   media, err := reader("mpd/amc.mpd")
+   reps, err := reader("mpd/amc.mpd")
    if err != nil {
       t.Fatal(err)
    }
-   media.Visit(func(p Pointer) {
-      v, ok := p.Initialization()
-      fmt.Printf("%v %q %v\n\n", p.Representation.ID, v, ok)
-   })
-}
-
-var tests = []string{
-   "mpd/amc.mpd",
-   "mpd/hulu.mpd",
-   "mpd/nbc.mpd",
-   "mpd/paramount.mpd",
-   "mpd/roku.mpd",
+   for _, rep := range reps {
+      v, ok := rep.Initialization()
+      fmt.Printf("%v %q %v\n\n", rep.ID, v, ok)
+   }
 }
 
 func TestProtection(t *testing.T) {
    for _, test := range tests {
-      text, err := os.ReadFile(test)
+      reps, err := reader(test)
       if err != nil {
          t.Fatal(err)
       }
-      var media MPD
-      if err := xml.Unmarshal(text, &media); err != nil {
-         t.Fatal(err)
-      }
-      media.Visit(func(p Pointer) {
-         _, pssh := p.PSSH()
-         _, kid := p.Default_KID()
+      for _, rep := range reps {
+         _, pssh := rep.PSSH()
+         _, kid := rep.Default_KID()
          fmt.Printf(
             "mpd:%v period:%q type:%v pssh:%v kid:%v\n",
-            test, p.Period.ID, p.mime_type(), pssh, kid,
+            test, rep.adaptation_set.period.ID, rep.mime_type(), pssh, kid,
          )
-      })
+      }
+   }
+}
+
+var media_tests = [][]string{
+   // startNumber == nil
+   {"mpd/mubi.mpd", "new-york-edge2.mubicdn.net/stream/43cac9f0138aaa566a429be4542ff21c/65df1dc5/728eb9fc/mubi-films/325455/passages_eng_zxx_1800x1080_50000_mezz40828/ae8c88ed4e/drm_playlist.0ff148ef80.ism/default/"},
+   // startNumber == 0
+   {"mpd/amc.mpd", ""},
+   // startNumber == 1
+   {"mpd/paramount.mpd", "vod-gcs-cedexis.cbsaavideo.com/intl_vms/2022/02/24/2006197315671/77016_cenc_dash/"},
+}
+
+func TestMedia(t *testing.T) {
+   for _, test := range media_tests {
+      fmt.Println(test[0] + ":")
+      reps, err := reader(test[0])
+      if err != nil {
+         t.Fatal(err)
+      }
+      for _, media := range reps[0].Media() {
+         fmt.Println(test[1] + media)
+      }
    }
 }
