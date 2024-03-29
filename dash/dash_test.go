8fc6d483b58d2642e543a9c62db6359da3fbc53f
3052 <spen+3052@protonmail.com>
2024-03-29 07:30:34 -0500

    new file:   dash/mpd/roku-clear.mpd

--- dash/dash_test.go
+++ dash/dash_test.go
@@ -30,28 +30,6 @@ func TestDelete(t *testing.T) {
    }
 }
 
-func TestMedia(t *testing.T) {
-   for _, test := range media_tests {
-      fmt.Println(test[0] + ":")
-      reps, err := reader(test[0])
-      if err != nil {
-         t.Fatal(err)
-      }
-      for _, media := range reps[0].Media() {
-         fmt.Println(test[1] + media)
-      }
-   }
-}
-
-var media_tests = [][]string{
-   // startNumber == nil
-   {"mpd/mubi.mpd", "new-york-edge2.mubicdn.net/stream/43cac9f0138aaa566a429be4542ff21c/65df1dc5/728eb9fc/mubi-films/325455/passages_eng_zxx_1800x1080_50000_mezz40828/ae8c88ed4e/drm_playlist.0ff148ef80.ism/default/"},
-   // startNumber == 0
-   {"mpd/amc.mpd", ""},
-   // startNumber == 1
-   {"mpd/paramount.mpd", "vod-gcs-cedexis.cbsaavideo.com/intl_vms/2022/02/24/2006197315671/77016_cenc_dash/"},
-}
-
 func TestInitialization(t *testing.T) {
    reps, err := reader("mpd/amc.mpd")
    if err != nil {
@@ -85,4 +63,29 @@ var tests = []string{
    "mpd/paramount.mpd",
    "mpd/roku.mpd",
    "mpd/stan.mpd",
+}
+
+var media_tests = []struct{
+   name string
+   base string
+}{
+   // startNumber == nil
+   {"mpd/mubi.mpd", "new-york-edge2.mubicdn.net/stream/43cac9f0138aaa566a429be4542ff21c/65df1dc5/728eb9fc/mubi-films/325455/passages_eng_zxx_1800x1080_50000_mezz40828/ae8c88ed4e/drm_playlist.0ff148ef80.ism/default/"},
+   // startNumber == 0
+   {"mpd/amc.mpd", ""},
+   // startNumber == 1
+   {"mpd/paramount.mpd", "vod-gcs-cedexis.cbsaavideo.com/intl_vms/2022/02/24/2006197315671/77016_cenc_dash/"},
+}
+
+func TestMedia(t *testing.T) {
+   for _, test := range media_tests {
+      fmt.Println(test.name + ":")
+      reps, err := reader(test.name)
+      if err != nil {
+         t.Fatal(err)
+      }
+      for _, media := range reps[0].Media() {
+         fmt.Println(test.base + media)
+      }
+   }
 }
--- dash/range.go
+++ dash/range.go
@@ -22,8 +22,9 @@ type Representation struct {
 
 type Range string
 
-func (r Range) Scan() (int, int, error) {
-   var start, end int
+// range-start and range-end can both exceed 32 bits, so we must use 64 bit
+func (r Range) Scan() (uint64, uint64, error) {
+   var start, end uint64
    _, err := fmt.Sscanf(string(r), "%v-%v", &start, &end)
    if err != nil {
       return 0, 0, err
