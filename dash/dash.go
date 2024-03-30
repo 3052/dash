d26cdc733160ef3bcf3b279d96e64dc20ace4aa3
3052 <spen+3052@protonmail.com>
2024-03-30 13:19:59 -0500

    new file:   dash/representation.go

--- dash/dash.go
+++ dash/dash.go
@@ -1,50 +1,13 @@
 package dash
 
 import (
-   "encoding/xml"
    "fmt"
    "strconv"
    "strings"
    "time"
 )
 
-// dashif-documents.azurewebsites.net/Guidelines-TimingModel/master/Guidelines-TimingModel.html#addressing-simple-to-explicit
-type mpd struct {
-   MediaPresentationDuration string `xml:"mediaPresentationDuration,attr"`
-   Period []period
-}
-
 type Range string
-
-func Unmarshal(b []byte) ([]Representation, error) {
-   var media mpd
-   err := xml.Unmarshal(b, &media)
-   if err != nil {
-      return nil, err
-   }
-   var rs []Representation
-   for _, p := range media.Period {
-      p.mpd = &media
-      for _, a := range p.AdaptationSet {
-         a.period = &p
-         for _, r := range a.Representation {
-            r.adaptation_set = &a
-            rs = append(rs, r)
-         }
-      }
-   }
-   return rs, nil
-}
-
-func (r Representation) Ext() (string, bool) {
-   switch r.GetMimeType() {
-   case "audio/mp4":
-      return ".m4a", true
-   case "video/mp4":
-      return ".m4v", true
-   }
-   return "", false
-}
 
 // range-start and range-end can both exceed 32 bits, so we must use 64 bit
 func (r Range) Scan() (uint64, uint64, error) {
@@ -56,41 +19,10 @@ func (r Range) Scan() (uint64, uint64, error) {
    return start, end, nil
 }
 
-func (r Representation) String() string {
-   var b []byte
-   if v := r.Width; v != nil {
-      b = append(b, "width = "...)
-      b = strconv.AppendInt(b, *v, 10)
-   }
-   if v := r.Height; v != nil {
-      if b != nil {
-         b = append(b, '\n')
-      }
-      b = append(b, "height = "...)
-      b = strconv.AppendInt(b, *v, 10)
-   }
-   if b != nil {
-      b = append(b, '\n')
-   }
-   b = append(b, "bandwidth = "...)
-   b = strconv.AppendInt(b, r.Bandwidth, 10)
-   if v, ok := r.GetCodecs(); ok {
-      b = append(b, "\ncodecs = "...)
-      b = append(b, v...)
-   }
-   b = append(b, "\ntype = "...)
-   b = append(b, r.GetMimeType()...)
-   if v := r.adaptation_set.Role; v != nil {
-      b = append(b, "\nrole = "...)
-      b = append(b, v.Value...)
-   }
-   if v := r.adaptation_set.Lang; v != nil {
-      b = append(b, "\nlang = "...)
-      b = append(b, *v...)
-   }
-   b = append(b, "\nid = "...)
-   b = append(b, r.ID...)
-   return string(b)
+// dashif-documents.azurewebsites.net/Guidelines-TimingModel/master/Guidelines-TimingModel.html#addressing-simple-to-explicit
+type mpd struct {
+   MediaPresentationDuration string `xml:"mediaPresentationDuration,attr"`
+   Period []period
 }
 
 func (m mpd) Seconds() (float64, error) {
@@ -100,23 +32,6 @@ func (m mpd) Seconds() (float64, error) {
       return 0, err
    }
    return duration.Seconds(), nil
-}
-
-func (r Representation) GetCodecs() (string, bool) {
-   if v := r.Codecs; v != nil {
-      return *v, true
-   }
-   if v := r.adaptation_set.Codecs; v != nil {
-      return *v, true
-   }
-   return "", false
-}
-
-func (r Representation) GetMimeType() string {
-   if v := r.MimeType; v != nil {
-      return *v
-   }
-   return *r.adaptation_set.MimeType
 }
 
 type period struct {
@@ -134,34 +49,6 @@ type adaptation_set struct {
    }
    SegmentTemplate *SegmentTemplate
    period *period
-}
-
-func (r Representation) GetSegmentTemplate() (*SegmentTemplate, bool) {
-   if v := r.SegmentTemplate; v != nil {
-      return v, true
-   }
-   if v := r.adaptation_set.SegmentTemplate; v != nil {
-      return v, true
-   }
-   return nil, false
-}
-
-type Representation struct {
-   Bandwidth int64 `xml:"bandwidth,attr"`
-   BaseURL *string
-   Codecs *string `xml:"codecs,attr"`
-   Height *int64 `xml:"height,attr"`
-   ID string `xml:"id,attr"`
-   MimeType *string `xml:"mimeType,attr"`
-   SegmentBase *struct {
-      IndexRange Range `xml:"indexRange,attr"`
-      Initialization struct {
-         Range Range `xml:"range,attr"`
-      }
-   }
-   SegmentTemplate *SegmentTemplate
-   Width *int64 `xml:"width,attr"`
-   adaptation_set *adaptation_set
 }
 
 func (s SegmentTemplate) GetInitialization(id string) (string, bool) {
--- dash/dash_test.go
+++ dash/dash_test.go
@@ -4,7 +4,6 @@ import (
    "encoding/xml"
    "fmt"
    "os"
-   "slices"
    "testing"
 )
 
@@ -130,8 +129,6 @@ func TestPeriod(t *testing.T) {
    }
 }
 
-type set map[byte]struct{}
-
 func TestAdaptation(t *testing.T) {
    sets := struct{
       codecs set
@@ -207,117 +204,4 @@ func TestAdaptation(t *testing.T) {
       }
    }
    fmt.Printf("%+v\n", sets)
-}
-
-func TestRepresentation(t *testing.T) {
-   sets := struct{
-      bandwidth set
-      base_url set
-      height set
-      id set
-      indexRange set
-      initialization set
-      segmentBase set
-      width set
-   }{
-      make(set),
-      make(set),
-      make(set),
-      make(set),
-      make(set),
-      make(set),
-      make(set),
-      make(set),
-   }
-   for _, test := range tests {
-      text, err := os.ReadFile(test)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var media mpd
-      if err := xml.Unmarshal(text, &media); err != nil {
-         t.Fatal(err)
-      }
-      for _, per := range media.Period {
-         for _, ada := range per.AdaptationSet {
-            for _, rep := range ada.Representation {
-               if rep.Bandwidth >= 1 {
-                  sets.bandwidth[1] = struct{}{}
-               } else {
-                  sets.bandwidth[0] = struct{}{}
-               }
-               if rep.BaseURL != nil {
-                  sets.base_url[1] = struct{}{}
-               } else {
-                  sets.base_url[0] = struct{}{}
-               }
-               if rep.Height != nil {
-                  sets.height[1] = struct{}{}
-               } else {
-                  sets.height[0] = struct{}{}
-               }
-               if rep.Width != nil {
-                  sets.width[1] = struct{}{}
-               } else {
-                  sets.width[0] = struct{}{}
-               }
-               if rep.ID != "" {
-                  sets.id[1] = struct{}{}
-               } else {
-                  sets.id[0] = struct{}{}
-               }
-               if v := rep.SegmentBase; v != nil {
-                  sets.segmentBase[1] = struct{}{}
-                  if v.Initialization.Range != "" {
-                     sets.initialization[1] = struct{}{}
-                  } else {
-                     sets.initialization[0] = struct{}{}
-                  }
-                  if v.IndexRange != "" {
-                     sets.indexRange[1] = struct{}{}
-                  } else {
-                     sets.indexRange[0] = struct{}{}
-                  }
-               } else {
-                  sets.segmentBase[0] = struct{}{}
-               }
-            }
-         }
-      }
-   }
-   fmt.Printf("%+v\n", sets)
-}
-
-func TestDelete(t *testing.T) {
-   for _, test := range tests {
-      text, err := os.ReadFile(test)
-      if err != nil {
-         t.Fatal(err)
-      }
-      reps, err := Unmarshal(text)
-      if err != nil {
-         t.Fatal(err)
-      }
-      reps = slices.DeleteFunc(reps, func(r Representation) bool {
-         if _, ok := r.Ext(); !ok {
-            return true
-         }
-         return false
-      })
-      for _, rep := range reps {
-         fmt.Println(rep)
-      }
-   }
-}
-
-var tests = []string{
-   "mpd/amc.mpd",
-   "mpd/hulu.mpd",
-   "mpd/mubi.mpd",
-   "mpd/nbc.mpd",
-   "mpd/paramount.mpd",
-   "mpd/peacock.mpd",
-   "mpd/roku-clear.mpd",
-   "mpd/roku-protected.mpd",
-   "mpd/stan.mpd",
 }
