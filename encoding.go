3ffd21eff0a1d2fbd7ea75f8d9aa97a850b36abd
3052 <spen+3052@protonmail.com>
2024-04-02 18:24:35 -0500

    deleted:    name.go

--- encoding.go
+++ encoding.go
@@ -1,44 +1,58 @@
 package encoding
 
-import "strconv"
+import (
+   "bytes"
+   "strconv"
+   "text/template"
+)
 
-type Cardinal float64
+const Format = 
+   "{{if .Show}}" +
+      "{{.Show}} - {{.Season}} {{.Episode}} - {{.Title}}" +
+   "{{else}}" +
+      "{{.Title}} - {{.Year}}" +
+   "{{end}}"
 
-type Rate float64
-
-type Size float64
-
-func (c Cardinal) String() string {
-   units := []unit_measure{
-      {1, ""},
-      {1e-3, " thousand"},
-      {1e-6, " million"},
-      {1e-9, " billion"},
-      {1e-12, " trillion"},
+func Name(format string, v Namer) (string, error) {
+   t, err := new(template.Template).Parse(format)
+   if err != nil {
+      return "", err
    }
-   return scale(float64(c), units)
+   var buf bytes.Buffer
+   if err := t.Execute(&buf, v); err != nil {
+      return "", err
+   }
+   bytes := buf.Bytes()
+   clean(bytes)
+   return string(bytes), nil
 }
 
-func (r Rate) String() string {
-   units := []unit_measure{
-      {1, " byte/s"},
-      {1e-3, " kilobyte/s"},
-      {1e-6, " megabyte/s"},
-      {1e-9, " gigabyte/s"},
-      {1e-12, " terabyte/s"},
+func clean(path []byte) {
+   m := map[byte]bool{
+      '"': true,
+      '*': true,
+      '/': true,
+      ':': true,
+      '<': true,
+      '>': true,
+      '?': true,
+      '\\': true,
+      '|': true,
    }
-   return scale(float64(r), units)
+   for k, v := range path {
+      if m[v] {
+         path[k] = '-'
+      }
+   }
 }
 
-func (s Size) String() string {
-   units := []unit_measure{
-      {1, " byte"},
-      {1e-3, " kilobyte"},
-      {1e-6, " megabyte"},
-      {1e-9, " gigabyte"},
-      {1e-12, " terabyte"},
+func label(value float64, unit unit_measure) string {
+   var prec int
+   if unit.factor != 1 {
+      prec = 2
+      value *= unit.factor
    }
-   return scale(float64(s), units)
+   return strconv.FormatFloat(value, 'f', prec, 64) + unit.name
 }
 
 func scale(value float64, units []unit_measure) string {
@@ -51,18 +65,25 @@ func scale(value float64, units []unit_measure) string {
    return label(value, unit)
 }
 
-type unit_measure struct {
-   factor float64
-   name string
+type Cardinal float64
+
+func (c Cardinal) String() string {
+   units := []unit_measure{
+      {1, ""},
+      {1e-3, " thousand"},
+      {1e-6, " million"},
+      {1e-9, " billion"},
+      {1e-12, " trillion"},
+   }
+   return scale(float64(c), units)
 }
 
-func label(value float64, unit unit_measure) string {
-   var prec int
-   if unit.factor != 1 {
-      prec = 2
-      value *= unit.factor
-   }
-   return strconv.FormatFloat(value, 'f', prec, 64) + unit.name
+type Namer interface {
+   Show() string
+   Season() int
+   Episode() int
+   Title() string
+   Year() int
 }
 
 type Percent float64
@@ -70,4 +91,35 @@ type Percent float64
 func (p Percent) String() string {
    unit := unit_measure{100, " %"}
    return label(float64(p), unit)
+}
+
+type Rate float64
+
+func (r Rate) String() string {
+   units := []unit_measure{
+      {1, " byte/s"},
+      {1e-3, " kilobyte/s"},
+      {1e-6, " megabyte/s"},
+      {1e-9, " gigabyte/s"},
+      {1e-12, " terabyte/s"},
+   }
+   return scale(float64(r), units)
+}
+
+type Size float64
+
+func (s Size) String() string {
+   units := []unit_measure{
+      {1, " byte"},
+      {1e-3, " kilobyte"},
+      {1e-6, " megabyte"},
+      {1e-9, " gigabyte"},
+      {1e-12, " terabyte"},
+   }
+   return scale(float64(s), units)
+}
+
+type unit_measure struct {
+   factor float64
+   name string
 }
