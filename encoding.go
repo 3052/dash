d17a92b949130bc0a1954e2e59cc310a1f0fd294
3052 <spen+3052@protonmail.com>
2024-04-01 20:52:25 -0500

    new file:   docs/internal/index.html

--- encoding.go
+++ encoding.go
@@ -4,34 +4,27 @@ import "strconv"
 
 type Namer interface {
    Owner() (string, bool)
-   Show() (string, bool)
    Season() (string, bool)
    Episode() (string, bool)
    Title() (string, bool)
    Year() (string, bool)
 }
 
-// Owner - Show Season Episode - Title - Year
+// Owner - Season - Episode - Title - Year
 func Name(n Namer) string {
    var b []byte
    if v, ok := n.Owner(); ok {
       b = append(b, v...)
    }
-   if v, ok := n.Show(); ok {
+   if v, ok := n.Season(); ok {
       if b != nil {
          b = append(b, " - "...)
       }
       b = append(b, v...)
    }
-   if v, ok := n.Season(); ok {
-      if b != nil {
-         b = append(b, ' ')
-      }
-      b = append(b, v...)
-   }
    if v, ok := n.Episode(); ok {
       if b != nil {
-         b = append(b, ' ')
+         b = append(b, " - "...)
       }
       b = append(b, v...)
    }
@@ -69,6 +62,7 @@ func clean(path []byte) {
       }
    }
 }
+
 type Cardinal float64
 
 type Rate float64
