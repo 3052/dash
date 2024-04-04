11c8742943e046d6d7cc70659f35093d1d7be698
3052 <spen+3052@protonmail.com>
2024-04-03 19:25:33 -0500

    new file:   name.go

---
+++ name_test.go
@@ -0,0 +1,63 @@
+package encoding
+
+import (
+   "fmt"
+   "testing"
+)
+
+func TestName(t *testing.T) {
+   namers := []Namer{
+      episode{}, film{},
+   }
+   for _, namer := range namers {
+      name, err := Name(namer)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Printf("%q\n", name)
+   }
+}
+
+type episode struct{}
+
+func (episode) Show() string {
+   return "show"
+}
+
+func (episode) Title() string {
+   return "title"
+}
+
+type film struct{}
+
+func (film) Show() string {
+   return ""
+}
+
+func (film) Title() string {
+   return "title"
+}
+
+func (episode) Season() int {
+   return 2
+}
+
+func (episode) Episode() int {
+   return 3
+}
+
+func (episode) Year() int {
+   return 2024
+}
+
+func (film) Season() int {
+   return 0
+}
+
+func (film) Episode() int {
+   return 0
+}
+
+func (film) Year() int {
+   return 2024
+}
