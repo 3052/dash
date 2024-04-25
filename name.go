80edef66f656ee476adbc3750f2e84e2f4e6228b
3052 <spen+3052@protonmail.com>
2024-04-25 07:47:55 -0500

    new file:   name.go

---
+++ name.go
@@ -0,0 +1,44 @@
+package encoding
+
+import (
+   "strings"
+   "text/template"
+)
+
+func Name(n Namer) (string, error) {
+   text, err := new(template.Template).Parse(Format)
+   if err != nil {
+      return "", err
+   }
+   var b strings.Builder
+   err = text.Execute(&b, n)
+   if err != nil {
+      return "", err
+   }
+   return b.String(), nil
+}
+
+type Namer interface {
+   Show() string
+   Season() int
+   Episode() int
+   Title() string
+   Year() int
+}
+
+var Format = 
+   "{{if .Show}}" +
+      "{{.Show}} - {{.Season}} {{.Episode}} - {{.Title}}" +
+   "{{else}}" +
+      "{{.Title}} - {{.Year}}" +
+   "{{end}}"
+
+func Clean(s string) string {
+   mapping := func(r rune) rune {
+      if strings.ContainsRune(`"*/:<>?\|`, r) {
+         return '-'
+      }
+      return r
+   }
+   return strings.Map(mapping, s)
+}
