4e90cd1be7c593b1574512d87085ace8810a4ff1
3052 <spen+3052@protonmail.com>
2024-05-26 22:35:49 -0500

    deleted:    encoding.go

--- dash/dash.go
+++ dash/dash.go
@@ -9,6 +9,26 @@ import (
    "time"
 )
 
+// filter out ads, for example:
+// hulu.com/watch/5add1b6c-04f2-4038-a925-35db3007d662
+func (p Period) Seconds() (float64, error) {
+   s := strings.TrimPrefix(p.get_duration(), "PT")
+   d, err := time.ParseDuration(strings.ToLower(s))
+   if err != nil {
+      return 0, err
+   }
+   return d.Seconds(), nil
+}
+
+type MPD struct {
+   BaseUrl *URL `xml:"BaseURL"`
+   MediaPresentationDuration string `xml:"mediaPresentationDuration,attr"`
+   Period []*Period
+}
+
+// content protection
+// github.com/3052/encoding/tree/da18a91/dash
+
 func (r *Range) UnmarshalText(text []byte) error {
    start, end, found := strings.Cut(string(text), "-")
    if !found {
@@ -39,9 +59,6 @@ type Range struct {
    Start uint64
    End uint64
 }
-
-// content protection
-// github.com/3052/encoding/tree/da18a91/dash
 
 type Representation struct {
    Bandwidth int64 `xml:"bandwidth,attr"`
@@ -174,15 +191,6 @@ func (r Representation) get_width() (int64, bool) {
    return 0, false
 }
 
-func (p Period) Seconds() (float64, error) {
-   s := strings.TrimPrefix(p.get_duration(), "PT")
-   d, err := time.ParseDuration(strings.ToLower(s))
-   if err != nil {
-      return 0, err
-   }
-   return d.Seconds(), nil
-}
-
 func (r Representation) GetAdaptationSet() *AdaptationSet {
    return r.adaptation_set
 }
@@ -213,12 +221,6 @@ type Period struct {
 
 func (p Period) GetMpd() *MPD {
    return p.mpd
-}
-
-type MPD struct {
-   BaseUrl *URL `xml:"BaseURL"`
-   MediaPresentationDuration string `xml:"mediaPresentationDuration,attr"`
-   Period []*Period
 }
 
 type URL struct {
