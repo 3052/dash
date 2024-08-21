c162b742b8a6bfa21f33b4432630d21653c20c4f
3052 <spen+3052@protonmail.com>
2024-08-20 20:22:15 -0500

    modified:   internal/mpd.go

--- internal/mpd.go
+++ internal/mpd.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"os"
-	"strings"
+   "os"
+   "strings"
 )
 
 const max = 5
 
 func main() {
-	file, err := os.Create("out.mpd")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	text, err := os.ReadFile("stream.mpd")
-	if err != nil {
-		panic(err)
-	}
-	var count int
-	for _, line := range strings.SplitAfter(string(text), "\n") {
-		if strings.Contains(line, "<S ") {
-			count++
-		}
-		if strings.Contains(line, "</SegmentTimeline>") {
-			count = 0
-		}
-		if count <= max {
-			file.WriteString(line)
-		}
-	}
+   file, err := os.Create("out.mpd")
+   if err != nil {
+      panic(err)
+   }
+   defer file.Close()
+   text, err := os.ReadFile("stream.mpd")
+   if err != nil {
+      panic(err)
+   }
+   var count int
+   for _, line := range strings.SplitAfter(string(text), "\n") {
+      if strings.Contains(line, "<S ") {
+         count++
+      }
+      if strings.Contains(line, "</SegmentTimeline>") {
+         count = 0
+      }
+      if count <= max {
+         file.WriteString(line)
+      }
+   }
 }
