aece46b36f51e05cabed93ba3ec2b67255ea41b4
3052 <spen+3052@protonmail.com>
2024-12-29 19:47:32 -0600

    +   "net/url"

--- dash.go
+++ dash.go
@@ -2,103 +2,10 @@ package dash
 
 import (
    "iter"
+   "net/url"
    "strconv"
    "strings"
 )
-
-type Period struct {
-   AdaptationSet []AdaptationSet
-   BaseUrl       string `xml:"BaseURL"`
-   Id            string `xml:"id,attr"`
-   mpd           *Mpd
-}
-
-func (r *Representation) initialization() (string, bool) {
-   if v := r.SegmentTemplate; v != nil {
-      if v := v.Initialization; v != "" {
-         return strings.Replace(v, "$RepresentationID$", r.Id, 1), true
-      }
-   }
-   return "", false
-}
-
-func (m Mpd) representation() iter.Seq[Representation] {
-   return func(yield func(Representation) bool) {
-      for _, p := range m.Period {
-         p.mpd = &m
-         for _, adapt := range p.AdaptationSet {
-            adapt.period = &p
-            for _, represent := range adapt.Representation {
-               if represent.Codecs == nil {
-                  represent.Codecs = adapt.Codecs
-               }
-               if represent.Height == nil {
-                  represent.Height = adapt.Height
-               }
-               if represent.MimeType == nil {
-                  represent.MimeType = adapt.MimeType
-               }
-               if represent.SegmentTemplate == nil {
-                  represent.SegmentTemplate = adapt.SegmentTemplate
-               }
-               if represent.Width == nil {
-                  represent.Width = adapt.Width
-               }
-               represent.adaptation_set = &adapt
-               if !yield(represent) {
-                  return
-               }
-            }
-         }
-      }
-   }
-}
-
-type Representation struct {
-   Bandwidth       int64   `xml:"bandwidth,attr"`
-   Codecs          *string `xml:"codecs,attr"`
-   Height          *int64  `xml:"height,attr"`
-   Id              string  `xml:"id,attr"`
-   MimeType        *string `xml:"mimeType,attr"`
-   SegmentTemplate *SegmentTemplate
-   Width           *int64 `xml:"width,attr"`
-   adaptation_set  *AdaptationSet
-}
-
-type SegmentTemplate struct {
-   Initialization  string `xml:"initialization,attr"`
-   SegmentTimeline *struct {
-      S []struct {
-         R int `xml:"r,attr"` // repeat
-      }
-   }
-}
-
-type AdaptationSet struct {
-   Codecs         *string `xml:"codecs,attr"`
-   Height         *int64  `xml:"height,attr"`
-   Lang           string  `xml:"lang,attr"`
-   MimeType       *string `xml:"mimeType,attr"`
-   Representation []Representation
-   Role           *struct {
-      Value string `xml:"value,attr"`
-   }
-   SegmentTemplate *SegmentTemplate
-   Width           *int64 `xml:"width,attr"`
-   period          *Period
-}
-
-func (r *Representation) seq() iter.Seq[Representation] {
-   return func(yield func(Representation) bool) {
-      for r2 := range r.adaptation_set.period.mpd.representation() {
-         if r2.Id == r.Id {
-            if !yield(r2) {
-               return
-            }
-         }
-      }
-   }
-}
 
 func (r *Representation) String() string {
    var b []byte
@@ -143,4 +50,102 @@ func (r *Representation) String() string {
 
 type Mpd struct {
    Period []Period
+}
+
+type Url struct {
+   Url url.URL
+}
+
+type AdaptationSet struct {
+   Codecs         *string `xml:"codecs,attr"`
+   Height         *int64  `xml:"height,attr"`
+   Lang           string  `xml:"lang,attr"`
+   MimeType       *string `xml:"mimeType,attr"`
+   Representation []Representation
+   Role           *struct {
+      Value string `xml:"value,attr"`
+   }
+   SegmentTemplate *SegmentTemplate
+   Width           *int64 `xml:"width,attr"`
+   period          *Period
+}
+
+func (b *Url) UnmarshalText(data []byte) error {
+   return b.Url.UnmarshalBinary(data)
+}
+
+type Period struct {
+   AdaptationSet []AdaptationSet
+   BaseUrl       *Url   `xml:"BaseURL"`
+   Id            string `xml:"id,attr"`
+   mpd           *Mpd
+}
+
+func (r *Representation) seq() iter.Seq[Representation] {
+   return func(yield func(Representation) bool) {
+      for r2 := range r.adaptation_set.period.mpd.representation() {
+         if r2.Id == r.Id {
+            if !yield(r2) {
+               return
+            }
+         }
+      }
+   }
+}
+
+type Representation struct {
+   Bandwidth       int64   `xml:"bandwidth,attr"`
+   Codecs          *string `xml:"codecs,attr"`
+   Height          *int64  `xml:"height,attr"`
+   Id              string  `xml:"id,attr"`
+   MimeType        *string `xml:"mimeType,attr"`
+   SegmentTemplate *SegmentTemplate
+   Width           *int64 `xml:"width,attr"`
+   adaptation_set  *AdaptationSet
+}
+
+type SegmentTemplate struct {
+   Initialization *Url `xml:"initialization,attr"`
+}
+
+func (r *Representation) initialization() (*Url, bool) {
+   if v := r.SegmentTemplate; v != nil {
+      if v := v.Initialization; v != nil {
+         v.Url.Path = strings.Replace(v.Url.Path, "$RepresentationID$", r.Id, 1)
+         return v, true
+      }
+   }
+   return nil, false
+}
+
+func (m Mpd) representation() iter.Seq[Representation] {
+   return func(yield func(Representation) bool) {
+      for _, p := range m.Period {
+         p.mpd = &m
+         for _, adapt := range p.AdaptationSet {
+            adapt.period = &p
+            for _, represent := range adapt.Representation {
+               if represent.Codecs == nil {
+                  represent.Codecs = adapt.Codecs
+               }
+               if represent.Height == nil {
+                  represent.Height = adapt.Height
+               }
+               if represent.MimeType == nil {
+                  represent.MimeType = adapt.MimeType
+               }
+               if represent.SegmentTemplate == nil {
+                  represent.SegmentTemplate = adapt.SegmentTemplate
+               }
+               if represent.Width == nil {
+                  represent.Width = adapt.Width
+               }
+               represent.adaptation_set = &adapt
+               if !yield(represent) {
+                  return
+               }
+            }
+         }
+      }
+   }
 }
