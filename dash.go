5eb4e8e2db9ed235ef31bb6ea7c6883ea11ab08a
3052 <spen+3052@protonmail.com>
2025-01-05 12:27:55 -0600

    +func (r *Range) UnmarshalText(data []byte) error {

--- dash.go
+++ dash.go
@@ -9,6 +9,33 @@ import (
    "time"
 )
 
+func (r *Range) UnmarshalText(data []byte) error {
+   before, after, _ := strings.Cut(string(data), "-")
+   var err error
+   if before != "" {
+      (*r)[0], err = strconv.ParseUint(before, 10, 64)
+      if err != nil {
+         return err
+      }
+   }
+   (*r)[1], err = strconv.ParseUint(after, 10, 64)
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+func (r Range) MarshalText() ([]byte, error) {
+   b := strconv.AppendUint(nil, r[0], 10)
+   b = append(b, '-')
+   return strconv.AppendUint(b, r[1], 10), nil
+}
+
+// SegmentIndexBox uses:
+// unsigned int(32) subsegment_duration;
+// but range values can exceed 32 bits
+type Range [2]uint64
+
 type SegmentTemplate struct {
    Initialization         Initialization `xml:"initialization,attr"`
    Media                  Media          `xml:"media,attr"`
@@ -128,32 +155,6 @@ func (p *Pssh) UnmarshalText(data []byte) error {
    }
    return nil
 }
-
-// the current testdata always has `-`, so lets assume for now
-func (r *Range) UnmarshalText(data []byte) error {
-   before, after, _ := strings.Cut(string(data), "-")
-   var err error
-   (*r)[0], err = strconv.ParseUint(before, 10, 64)
-   if err != nil {
-      return err
-   }
-   (*r)[1], err = strconv.ParseUint(after, 10, 64)
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-func (r Range) MarshalText() ([]byte, error) {
-   b := strconv.AppendUint(nil, r[0], 10)
-   b = append(b, '-')
-   return strconv.AppendUint(b, r[1], 10), nil
-}
-
-// SegmentIndexBox uses:
-// unsigned int(32) subsegment_duration;
-// but range values can exceed 32 bits
-type Range [2]uint64
 
 func (s SchemeIdUri) Widevine() bool {
    return s == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
--- dash_test.go
+++ dash_test.go
@@ -1,10 +1,92 @@
 package dash
 
 import (
-   "fmt"
+   "encoding/xml"
+   "log"
+   "net/http"
+   "net/url"
+   "os"
    "testing"
 )
 
+var range_tests = []struct{
+   data string
+   ok bool
+}{
+   {"-3", true},
+   {"2-", false},
+   {"2-3", true},
+}
+
+func TestRange(t *testing.T) {
+   for _, test := range range_tests {
+      var r Range
+      ok := r.UnmarshalText([]byte(test.data)) == nil
+      if ok != test.ok {
+         t.Fatal(test)
+      }
+   }
+}
+
+func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
+   log.Print(req.URL)
+   return http.DefaultTransport.RoundTrip(req)
+}
+
+type transport struct{}
+
+func TestInitialization(t *testing.T) {
+   data, err := os.ReadFile("ignore/pluto.mpd")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var media Mpd
+   err = xml.Unmarshal(data, &media)
+   if err != nil {
+      t.Fatal(err)
+   }
+   base, err := url.Parse(pluto_mpd)
+   if err != nil {
+      t.Fatal(err)
+   }
+   media.Set(base)
+   var represent Representation
+   for represent = range media.Representation() {
+      if *represent.MimeType == "video/mp4" {
+         break
+      }
+   }
+   initial, err := represent.SegmentTemplate.Initialization.Url(&represent)
+   if err != nil {
+      t.Fatal(err)
+   }
+   http.DefaultClient.Transport = transport{}
+   resp, err := http.Head(initial.String())
+   if err != nil {
+      t.Fatal(err)
+   }
+   if resp.StatusCode != http.StatusOK {
+      t.Fatal(resp.Status)
+   }
+}
+
+func TestPssh(t *testing.T) {
+   var p Pssh
+   if p.UnmarshalText([]byte{0}) == nil {
+      t.Fatal("Pssh.UnmarshalText")
+   }
+}
+
+func Test2Initialization(t *testing.T) {
+   _, err := Initialization{"\n"}.Url(&Representation{})
+   if err == nil {
+      t.Fatal("Initialization.Url")
+   }
+}
+
 func TestDuration(t *testing.T) {
-   fmt.Println("hello world")
+   var d Duration
+   if d.UnmarshalText(nil) == nil {
+      t.Fatal("Duration.UnmarshalText")
+   }
 }
