911c4bab4466a3f757a5fdc51e0e60bb9887436a
3052 <spen+3052@protonmail.com>
2025-01-05 08:33:09 -0600

    new file:   period.go

--- dash.go
+++ dash.go
@@ -3,7 +3,6 @@ package dash
 import (
    "encoding/base64"
    "iter"
-   "math"
    "net/url"
    "strconv"
    "strings"
@@ -94,34 +93,6 @@ func (m *Mpd) representation() iter.Seq[Representation] {
             }
          }
       }
-   }
-}
-
-// dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
-func (p *Period) segment_count(template *SegmentTemplate) float64 {
-   return math.Ceil(
-      *template.Timescale * p.Duration.D.Seconds() / template.Duration,
-   )
-}
-
-type Period struct {
-   AdaptationSet []AdaptationSet
-   BaseUrl       *Url      `xml:"BaseURL"`
-   Duration      *Duration `xml:"duration,attr"`
-   Id            string    `xml:"id,attr"`
-   mpd           *Mpd
-}
-
-func (p *Period) set(media *Mpd) {
-   p.mpd = media
-   if v := p.mpd.BaseUrl; v != nil {
-      if p.BaseUrl == nil {
-         p.BaseUrl = &Url{&url.URL{}}
-      }
-      p.BaseUrl.Url = v.Url.ResolveReference(p.BaseUrl.Url)
-   }
-   if p.Duration == nil {
-      p.Duration = p.mpd.MediaPresentationDuration
    }
 }
 
