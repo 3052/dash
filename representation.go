31f78ba40b85e738df338ef15172fd3d622f09a6
3052 <spen+3052@protonmail.com>
2025-01-06 05:54:46 -0600

    deleted:    internal/dash.go

--- representation.go
+++ representation.go
@@ -6,12 +6,76 @@ import (
    "strconv"
 )
 
+func (r *Representation) String() string {
+   var b []byte
+   if r.Width != nil {
+      b = append(b, "width = "...)
+      b = strconv.AppendInt(b, *r.Width, 10)
+   }
+   if r.Height != nil {
+      if b != nil {
+         b = append(b, '\n')
+      }
+      b = append(b, "height = "...)
+      b = strconv.AppendInt(b, *r.Height, 10)
+   }
+   if b != nil {
+      b = append(b, '\n')
+   }
+   b = append(b, "bandwidth = "...)
+   b = strconv.AppendInt(b, int64(r.Bandwidth), 10)
+   if r.Codecs != nil {
+      b = append(b, "\ncodecs = "...)
+      b = append(b, *r.Codecs...)
+   }
+   b = append(b, "\nmimeType = "...)
+   b = append(b, *r.MimeType...)
+   if role := r.adaptation_set.Role; role != nil {
+      b = append(b, "\nrole = "...)
+      b = append(b, role.Value...)
+   }
+   if lang := r.adaptation_set.Lang; lang != "" {
+      b = append(b, "\nlang = "...)
+      b = append(b, lang...)
+   }
+   if id := r.adaptation_set.period.Id; id != "" {
+      b = append(b, "\nperiod = "...)
+      b = append(b, id...)
+   }
+   b = append(b, "\nid = "...)
+   b = append(b, r.Id...)
+   return string(b)
+}
+
+type Representation struct {
+   Bandwidth         int    `xml:"bandwidth,attr"`
+   Id                string `xml:"id,attr"`
+   SegmentTemplate   *SegmentTemplate
+   BaseUrl           *Url    `xml:"BaseURL"`
+   Codecs            *string `xml:"codecs,attr"`
+   ContentProtection []ContentProtection
+   Height            *int64  `xml:"height,attr"`
+   MimeType          *string `xml:"mimeType,attr"`
+   SegmentBase       *SegmentBase
+   Width             *int64 `xml:"width,attr"`
+   adaptation_set    *AdaptationSet
+}
+
 func (r *Representation) Representation() iter.Seq[Representation] {
    return func(yield func(Representation) bool) {
-      for r2 := range r.adaptation_set.period.mpd.Representation() {
-         if r2.Id == r.Id {
-            if !yield(r2) {
-               return
+      for _, p := range r.adaptation_set.period.mpd.Period {
+         for _, adapt := range p.AdaptationSet {
+            for _, represent := range adapt.Representation {
+               if represent.Id == r.Id {
+                  if adapt.period == nil {
+                     p.set(r.adaptation_set.period.mpd)
+                     adapt.set(&p)
+                  }
+                  represent.set(&adapt)
+                  if !yield(represent) {
+                     return
+                  }
+               }
             }
          }
       }
@@ -81,64 +145,4 @@ func (r *Representation) set(adapt *AdaptationSet) {
    if r.Width == nil {
       r.Width = r.adaptation_set.Width
    }
-}
-
-func (r *Representation) String() string {
-   var b []byte
-   if r.Width != nil {
-      b = append(b, "width = "...)
-      b = strconv.AppendInt(b, *r.Width, 10)
-   }
-   if r.Height != nil {
-      if b != nil {
-         b = append(b, '\n')
-      }
-      b = append(b, "height = "...)
-      b = strconv.AppendInt(b, *r.Height, 10)
-   }
-   if b != nil {
-      b = append(b, '\n')
-   }
-   b = append(b, "bandwidth = "...)
-   b = strconv.AppendInt(b, r.Bandwidth, 10)
-   if r.Codecs != nil {
-      b = append(b, "\ncodecs = "...)
-      b = append(b, *r.Codecs...)
-   }
-   b = append(b, "\nmimeType = "...)
-   b = append(b, *r.MimeType...)
-   if role := r.adaptation_set.Role; role != nil {
-      b = append(b, "\nrole = "...)
-      b = append(b, role.Value...)
-   }
-   if lang := r.adaptation_set.Lang; lang != "" {
-      b = append(b, "\nlang = "...)
-      b = append(b, lang...)
-   }
-   if id := r.adaptation_set.period.Id; id != "" {
-      b = append(b, "\nperiod = "...)
-      b = append(b, id...)
-   }
-   b = append(b, "\nid = "...)
-   b = append(b, r.Id...)
-   return string(b)
-}
-
-type Representation struct {
-   SegmentTemplate   *SegmentTemplate
-   Bandwidth         int64   `xml:"bandwidth,attr"`
-   BaseUrl           *Url    `xml:"BaseURL"`
-   Codecs            *string `xml:"codecs,attr"`
-   ContentProtection []ContentProtection
-   Height            *int64  `xml:"height,attr"`
-   Id                string  `xml:"id,attr"`
-   MimeType          *string `xml:"mimeType,attr"`
-   SegmentBase       *struct {
-      Initialization struct {
-         Range Range `xml:"range,attr"`
-      }
-      IndexRange Range `xml:"indexRange,attr"`
-   }
-   Width          *int64 `xml:"width,attr"`
-   adaptation_set *AdaptationSet
 }
