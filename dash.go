5730e00f48f349dd0e1aa6c6d0627147d9b1c261
3052 <spen+3052@protonmail.com>
2025-01-07 20:48:29 -0600

    new file:   template.go

--- dash.go
+++ dash.go
@@ -3,7 +3,6 @@ package dash
 import (
    "encoding/base64"
    "encoding/xml"
-   "fmt"
    "iter"
    "math"
    "net/url"
@@ -13,14 +12,7 @@ import (
 )
 
 type Representation struct {
-   Bandwidth         int     `xml:"bandwidth,attr"`
-   BaseUrl           *Url    `xml:"BaseURL"`
-   Codecs            *string `xml:"codecs,attr"`
-   ContentProtection []ContentProtection
-   Height            *int64  `xml:"height,attr"`
-   Id                string  `xml:"id,attr"`
-   MimeType          *string `xml:"mimeType,attr"`
-   SegmentBase       *SegmentBase
+   SegmentTemplate *SegmentTemplate
    SegmentList       *struct {
       Initialization struct {
          SourceUrl string `xml:"sourceURL,attr"`
@@ -29,7 +21,14 @@ type Representation struct {
          Media string `xml:"media,attr"`
       } `xml:"SegmentURL"`
    }
-   SegmentTemplate *SegmentTemplate
+   Bandwidth         int     `xml:"bandwidth,attr"`
+   BaseUrl           *Url    `xml:"BaseURL"`
+   Codecs            *string `xml:"codecs,attr"`
+   ContentProtection []ContentProtection
+   Height            *int64  `xml:"height,attr"`
+   Id                string  `xml:"id,attr"`
+   MimeType          *string `xml:"mimeType,attr"`
+   SegmentBase       *SegmentBase
    Width           *int64 `xml:"width,attr"`
    adaptation_set  *AdaptationSet
 }
@@ -160,6 +159,7 @@ func (r *Representation) set(adapt *AdaptationSet) {
       r.Width = r.adaptation_set.Width
    }
 }
+
 // dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
 func (p *Period) segment_count(template *SegmentTemplate) float64 {
    return math.Ceil(
@@ -187,43 +187,7 @@ func (p *Period) set(media *Mpd) {
       p.Duration = p.mpd.MediaPresentationDuration
    }
 }
-func (m Media) Url(r *Representation, i int) (*url.URL, error) {
-   replace(&m.S, "$RepresentationID$", r.Id)
-   if m.time() {
-      replace(&m.S, "$Time$", fmt.Sprint(i))
-   } else {
-      replace(&m.S, "$Number$", fmt.Sprint(i))
-      replace(&m.S, "$Number%02d$", fmt.Sprintf("%02d", i))
-      replace(&m.S, "$Number%03d$", fmt.Sprintf("%03d", i))
-      replace(&m.S, "$Number%04d$", fmt.Sprintf("%04d", i))
-      replace(&m.S, "$Number%05d$", fmt.Sprintf("%05d", i))
-      replace(&m.S, "$Number%06d$", fmt.Sprintf("%06d", i))
-      replace(&m.S, "$Number%07d$", fmt.Sprintf("%07d", i))
-      replace(&m.S, "$Number%08d$", fmt.Sprintf("%08d", i))
-      replace(&m.S, "$Number%09d$", fmt.Sprintf("%09d", i))
-   }
-   u, err := url.Parse(m.S)
-   if err != nil {
-      return nil, err
-   }
-   if r.BaseUrl != nil {
-      u = r.BaseUrl.Url.ResolveReference(u)
-   }
-   return u, nil
-}
 
-func (m Media) time() bool {
-   return strings.Contains(m.S, "$Time$")
-}
-
-type Media struct {
-   S string
-}
-
-func (m *Media) UnmarshalText(data []byte) error {
-   m.S = string(data)
-   return nil
-}
 func (m *Mpd) Unmarshal(data []byte) error {
    return xml.Unmarshal(data, m)
 }
@@ -279,36 +243,6 @@ func (s SchemeIdUri) Widevine() bool {
 }
 
 type SchemeIdUri string
-
-func (s *SegmentTemplate) set() {
-   // dashif.org/Guidelines-TimingModel#addressing-simple
-   if s.StartNumber == nil {
-      value := 1
-      s.StartNumber = &value
-   }
-   // dashif.org/Guidelines-TimingModel#timing-sampletimeline
-   if s.Timescale == nil {
-      var value uint64 = 1
-      s.Timescale = &value
-   }
-}
-
-type SegmentTemplate struct {
-   Initialization *Initialization `xml:"initialization,attr"`
-   Media          Media           `xml:"media,attr"`
-   Duration       float64         `xml:"duration,attr"`
-   // This can be any frequency but typically is the media clock frequency of
-   // one of the media streams (or a positive integer multiple thereof).
-   Timescale              *uint64 `xml:"timescale,attr"`
-   StartNumber            *int    `xml:"startNumber,attr"`
-   PresentationTimeOffset int     `xml:"presentationTimeOffset,attr"`
-   SegmentTimeline        *struct {
-      S []struct {
-         D int `xml:"d,attr"` // duration
-         R int `xml:"r,attr"` // repeat
-      }
-   }
-}
 
 func (b *Url) UnmarshalText(data []byte) error {
    if b.Url == nil {
@@ -375,27 +309,6 @@ func (d *Duration) UnmarshalText(data []byte) error {
 
 type Duration struct {
    D time.Duration
-}
-
-type Initialization struct {
-   S string
-}
-
-func (i *Initialization) UnmarshalText(data []byte) error {
-   i.S = string(data)
-   return nil
-}
-
-func (i Initialization) Url(r *Representation) (*url.URL, error) {
-   replace(&i.S, "$RepresentationID$", r.Id)
-   u, err := url.Parse(i.S)
-   if err != nil {
-      return nil, err
-   }
-   if r.BaseUrl != nil {
-      u = r.BaseUrl.Url.ResolveReference(u)
-   }
-   return u, nil
 }
 
 func (m *Mpd) Representation() iter.Seq[Representation] {
--- dash_test.go
+++ dash_test.go
@@ -54,6 +54,7 @@ func TestRepresentation(t *testing.T) {
       }
    })
 }
+
 func TestPeriod(t *testing.T) {
    data, err := os.ReadFile("testdata/max.mpd")
    if err != nil {
@@ -77,39 +78,6 @@ func TestPeriod(t *testing.T) {
    }
 }
 
-func TestMedia(t *testing.T) {
-   _, err := Media{"\n"}.Url(&Representation{}, 0)
-   if err == nil {
-      t.Fatal("Media")
-   }
-   data, err := os.ReadFile("testdata/itv.mpd")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var media Mpd
-   err = media.Unmarshal(data)
-   if err != nil {
-      t.Fatal(err)
-   }
-   var represent Representation
-   for represent = range media.Representation() {
-      break
-   }
-   _, err = represent.SegmentTemplate.Media.Url(&represent, 0)
-   if err != nil {
-      t.Fatal(err)
-   }
-}
-
-var pluto = struct {
-   init  string
-   media string
-   mpd   string
-}{
-   mpd:   "http://silo-hybrik.pluto.tv.s3.amazonaws.com/576_pluto/clip/64ff3987cecd3f001332df52_Memento/720pDRM/20230911_090007/dash/0-end/main.mpd",
-   init:  "http://silo-hybrik.pluto.tv.s3.amazonaws.com/576_pluto/clip/64ff3987cecd3f001332df52_Memento/720pDRM/20230911_090007/dash/0-end/video/240p-300/init.mp4",
-   media: "http://silo-hybrik.pluto.tv.s3.amazonaws.com/576_pluto/clip/64ff3987cecd3f001332df52_Memento/720pDRM/20230911_090007/dash/0-end/video/240p-300/01362.m4s",
-}
 func TestMpd(t *testing.T) {
    data, err := os.ReadFile("testdata/pluto.mpd")
    if err != nil {
@@ -126,40 +94,6 @@ func TestDuration(t *testing.T) {
    var d Duration
    if d.UnmarshalText(nil) == nil {
       t.Fatal("Duration.UnmarshalText")
-   }
-}
-
-func TestInitialization(t *testing.T) {
-   data, err := os.ReadFile("testdata/pluto.mpd")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var media Mpd
-   media.BaseUrl = &Url{}
-   media.BaseUrl.Url, err = url.Parse(pluto.mpd)
-   if err != nil {
-      t.Fatal(err)
-   }
-   err = media.Unmarshal(data)
-   if err != nil {
-      t.Fatal(err)
-   }
-   var represent Representation
-   for represent = range media.Representation() {
-      if *represent.MimeType == "video/mp4" {
-         break
-      }
-   }
-   initial, err := represent.SegmentTemplate.Initialization.Url(&represent)
-   if err != nil {
-      t.Fatal(err)
-   }
-   if initial.String() != pluto.init {
-      t.Fatal(initial)
-   }
-   _, err = Initialization{"\n"}.Url(&Representation{})
-   if err == nil {
-      t.Fatal("Initialization.Url")
    }
 }
 
