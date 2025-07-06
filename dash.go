796131fbcabfabf04cd5d8e6b5ebfcb995c89808
367 <367@tuta.io>
2025-07-06 16:38:42 -0500

    deleted:    SegmentTemplate.go

--- dash.go
+++ dash.go
@@ -9,6 +9,88 @@ import (
    "time"
 )
 
+type SegmentTemplate struct {
+   EndNumber              int            `xml:"endNumber,attr"`
+   Initialization         Initialization `xml:"initialization,attr"`
+   Media                  Media          `xml:"media,attr"`
+   PresentationTimeOffset int            `xml:"presentationTimeOffset,attr"`
+   SegmentTimeline        *struct {
+      S []struct {
+         D int `xml:"d,attr"` // duration
+         R int `xml:"r,attr"` // repeat
+      }
+   }
+   StartNumber *int `xml:"startNumber,attr"`
+   Duration    int  `xml:"duration,attr"`
+   // This can be any frequency but typically is the media clock frequency of
+   // one of the media streams (or a positive integer multiple thereof).
+   Timescale *int `xml:"timescale,attr"`
+}
+
+func (s *SegmentTemplate) Segment(periodVar *Period) iter.Seq[int] {
+   var address int
+   if s.Media.time_address() {
+      address = s.PresentationTimeOffset
+   } else {
+      address = *s.StartNumber
+   }
+   return func(yield func(int) bool) {
+      if s.EndNumber >= 1 {
+         for address <= s.EndNumber {
+            if !yield(address) {
+               return
+            }
+            address++
+         }
+      } else if s.SegmentTimeline != nil {
+         for _, segment := range s.SegmentTimeline.S {
+            for range 1 + segment.R {
+               if !yield(address) {
+                  return
+               }
+               if s.Media.time_address() {
+                  address += segment.D
+               } else {
+                  address++
+               }
+            }
+         }
+      } else {
+         for range periodVar.segment_count(s) {
+            if !yield(address) {
+               return
+            }
+            address++
+         }
+      }
+   }
+}
+
+func (s *SegmentTemplate) set() {
+   // dashif.org/Guidelines-TimingModel#addressing-simple
+   if s.StartNumber == nil {
+      start := 1
+      s.StartNumber = &start
+   }
+   // dashif.org/Guidelines-TimingModel#timing-sampletimeline
+   if s.Timescale == nil {
+      scale := 1
+      s.Timescale = &scale
+   }
+}
+// dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
+// SegmentCount = Ceil((AsSeconds(Period@duration)) /
+// (SegmentTemplate@duration / SegmentTemplate@timescale))
+func (p *Period) segment_count(template *SegmentTemplate) int64 {
+   // amc
+   // draken
+   // kanopy
+   // max
+   // paramount
+   durationVar := float64(template.Duration) / float64(*template.Timescale)
+   return int64(math.Ceil(p.Duration[0].Seconds() / durationVar))
+}
+
 // SegmentTemplate
 // dashif.org/Guidelines-TimingModel#addressing-explicit
 // dashif.org/Guidelines-TimingModel#addressing-simple
@@ -80,19 +162,6 @@ func (m Media) Url(represent *Representation, address int) (*url.URL, error) {
       urlVar = represent.BaseUrl[0].ResolveReference(urlVar)
    }
    return urlVar, nil
-}
-
-// dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
-// SegmentCount = Ceil((AsSeconds(Period@duration)) /
-// (SegmentTemplate@duration / SegmentTemplate@timescale))
-func (p *Period) segment_count(template *SegmentTemplate) int64 {
-   // amc
-   // draken
-   // kanopy
-   // max
-   // paramount
-   durationVar := float64(template.Duration) / float64(*template.Timescale)
-   return int64(math.Ceil(p.Duration[0].Seconds() / durationVar))
 }
 
 func (p *Period) set(mpdVar *Mpd) {
