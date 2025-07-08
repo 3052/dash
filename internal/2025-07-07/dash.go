b25af1a66044de338b07d8686679e7bea6449e28
367 <367@tuta.io>
2025-07-07 22:49:46 -0500

    deleted:    internal/2025-07-06/dash.go

--- internal/2025-07-07/dash.go
+++ internal/2025-07-07/dash.go
@@ -1,23 +1,11 @@
 package dash
 
 import (
+   "log"
    "math"
    "strings"
    "time"
 )
-
-func (s *SegmentTemplate) SegmentParameter(periodVar *Period) []segmentParameter {
-   if s.EndNumber >= 1 {
-      return s.byEndNumber()
-   }
-   if s.SegmentTimeline != nil {
-      if strings.Contains(s.Media, "$Time$") {
-         return s.byTimelineTime()
-      }
-      return s.byTimelineNumber()
-   }
-   return s.byPeriod(periodVar)
-}
 
 type SegmentBase struct {
    Initialization struct {
@@ -41,72 +29,14 @@ type Period struct {
    Id            string   `xml:"id,attr"`
 }
 
-func (s *SegmentTemplate) byEndNumber() []segmentParameter {
-   var segment []segmentParameter
-   number := *s.StartNumber
-   for number <= s.EndNumber {
-      segment = append(segment, segmentParameter{number: number})
-      number++
-   }
-   return segment
-}
-
-func (s *SegmentTemplate) byTimelineNumber() []segmentParameter {
-   var segments []segmentParameter
-   number := *s.StartNumber
-   for _, segment := range s.SegmentTimeline.S {
-      for range 1 + segment.R {
-         segments = append(segments, segmentParameter{number: number})
-         number++
-      }
-   }
-   return segments
-}
-
-type segmentParameter struct {
-   number uint
-   time   uint
-}
-
-func (s *SegmentTemplate) byTimelineTime() []segmentParameter {
-   var segments []segmentParameter
-   number := s.PresentationTimeOffset
-   for _, segment := range s.SegmentTimeline.S {
-      for range 1 + segment.R {
-         segments = append(segments, segmentParameter{time: number})
-         number += segment.D
-      }
-   }
-   return segments
-}
-
-func (s *SegmentTemplate) byPeriod(periodVar *Period) []segmentParameter {
-   var segment []segmentParameter
-   // dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
-   // SegmentCount = Ceil(
-   //    AsSeconds(Period@duration) /
-   //    (SegmentTemplate@duration / SegmentTemplate@timescale)
-   // )
-   segmentCount := int64(math.Ceil(
-      periodVar.Duration[0].Seconds() /
-         (float64(s.Duration) / float64(*s.Timescale)),
-   ))
-   number := *s.StartNumber
-   for range segmentCount {
-      segment = append(segment, segmentParameter{number: number})
-      number++
-   }
-   return segment
-}
-
 type Representation struct {
    Codecs          string `xml:"codecs,attr"`
-   Id              string  `xml:"id,attr"`
+   Id              string `xml:"id,attr"`
    MimeType        string `xml:"mimeType,attr"`
-   BaseUrl         string  `xml:"BaseURL"`
-   Bandwidth       uint  `xml:"bandwidth,attr"`
-   Width           uint `xml:"width,attr"`
-   Height          uint `xml:"height,attr"`
+   BaseUrl         string `xml:"BaseURL"`
+   Bandwidth       uint   `xml:"bandwidth,attr"`
+   Width           uint   `xml:"width,attr"`
+   Height          uint   `xml:"height,attr"`
    SegmentTemplate *SegmentTemplate
    SegmentList     *SegmentList
    SegmentBase     *SegmentBase
@@ -140,8 +70,8 @@ type SegmentTemplate struct {
 type AdaptationSet struct {
    Codecs         string `xml:"codecs,attr"`
    Height         uint   `xml:"height,attr"`
-   Lang           string  `xml:"lang,attr"`
-   MimeType       string  `xml:"mimeType,attr"`
+   Lang           string `xml:"lang,attr"`
+   MimeType       string `xml:"mimeType,attr"`
    Representation []Representation
    Role           *struct {
       Value string `xml:"value,attr"`
@@ -150,3 +80,111 @@ type AdaptationSet struct {
    Width           uint `xml:"width,attr"`
 }
 
+func (*SegmentBase) segments() []string {
+   return nil
+}
+
+func (s *SegmentList) segments() []string {
+   var segments []string
+   for _, segment := range s.SegmentUrl {
+      segments = append(segments, segment.Media)
+   }
+   return segments
+}
+
+func (r *Representation) Segments(
+   adapt *AdaptationSet, periodVar *Period,
+) []string {
+   if r.SegmentBase != nil {
+      return r.SegmentBase.segments()
+   }
+   if r.SegmentList != nil {
+      return r.SegmentList.segments()
+   }
+   if r.SegmentTemplate != nil {
+      return r.SegmentTemplate.segments(periodVar)
+   }
+   return adapt.SegmentTemplate.segments(periodVar)
+}
+
+type segmentParameter struct {
+   number           uint
+   RepresentationId string
+   time             uint
+}
+
+///
+
+func (s *SegmentTemplate) byEndNumber() []segmentParameter {
+   var segment []segmentParameter
+   number := *s.StartNumber
+   for number <= s.EndNumber {
+      segment = append(segment, segmentParameter{number: number})
+      number++
+   }
+   return segment
+}
+
+func (s *SegmentTemplate) byTimelineNumber() []segmentParameter {
+   var segments []segmentParameter
+   number := *s.StartNumber
+   for _, segment := range s.SegmentTimeline.S {
+      for range 1 + segment.R {
+         segments = append(segments, segmentParameter{number: number})
+         number++
+      }
+   }
+   return segments
+}
+
+func (s *SegmentTemplate) byTimelineTime() []segmentParameter {
+   var segments []segmentParameter
+   number := s.PresentationTimeOffset
+   for _, segment := range s.SegmentTimeline.S {
+      for range 1 + segment.R {
+         segments = append(segments, segmentParameter{time: number})
+         number += segment.D
+      }
+   }
+   return segments
+}
+
+func (s *SegmentTemplate) byPeriod(periodVar *Period) []segmentParameter {
+   var segment []segmentParameter
+   // dashif.org/Guidelines-TimingModel#addressing-simple-to-explicit
+   // SegmentCount = Ceil(
+   //    AsSeconds(Period@duration) /
+   //    (SegmentTemplate@duration / SegmentTemplate@timescale)
+   // )
+   segmentCount := int64(math.Ceil(
+      periodVar.Duration[0].Seconds() /
+         (float64(s.Duration) / float64(*s.Timescale)),
+   ))
+   number := *s.StartNumber
+   for range segmentCount {
+      segment = append(segment, segmentParameter{number: number})
+      number++
+   }
+   return segment
+}
+
+func (s *SegmentTemplate) segmentParameter(periodVar *Period) []segmentParameter {
+   if s.EndNumber >= 1 {
+      return s.byEndNumber()
+   }
+   if s.SegmentTimeline != nil {
+      if strings.Contains(s.Media, "$Time$") {
+         return s.byTimelineTime()
+      }
+      return s.byTimelineNumber()
+   }
+   return s.byPeriod(periodVar)
+}
+
+func (s *SegmentTemplate) segments(periodVar *Period) []string {
+   var segments []string
+   for _, segment := range s.segmentParameter(periodVar) {
+      log.Print(segment)
+   }
+   return segments
+}
