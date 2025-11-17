de2f99f2dce4cb1db6d7daa75e67b77dd3e8495e
27 <27@riseup.net>
2025-11-16 18:26:49 -0600

    deleted:    internal/dash/adaptationSet.go

--- internal/dash/period.go
+++
@@ -1,9 +0,0 @@
-package dash
-
-import "encoding/xml"
-
-// Period represents a Period element in the MPD.
-type Period struct {
-   XMLName        xml.Name         `xml:"Period"`
-   AdaptationSets []*AdaptationSet `xml:"AdaptationSet,omitempty"`
-}
