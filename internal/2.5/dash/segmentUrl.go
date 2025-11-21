3d2c5114736353d07057c0ddab750324d597d976
27 <27@riseup.net>
2025-11-20 20:15:06 -0600

    deleted:    internal/2.5/dash/cencPssh.go

--- internal/2.5/dash/segmentUrl.go
+++
@@ -1,9 +0,0 @@
-package dash
-
-import "encoding/xml"
-
-// SegmentURL represents the SegmentURL element, which provides the URL for a media segment.
-type SegmentURL struct {
-   XMLName xml.Name `xml:"SegmentURL"`
-   Media   string   `xml:"media,attr"`
-}
